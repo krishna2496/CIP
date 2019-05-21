a5070cc215f75202a0a50bfe67f028c54608eb73
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-05-21 15:34:20 +0530

    update migrations

--- database/migrations/2019_05_16_060455_create_table_user_availability.php
+++ database/migrations/2019_05_16_060455_create_table_user_availability.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableUserAvailabilitie extends Migration
+class CreateTableUserAvailability extends Migration
 {
     /**
      * Run the migrations.
