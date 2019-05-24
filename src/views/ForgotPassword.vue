9c6b533f1f42e379b59f6ebeb7549b62a60cc507
Urvisha <urvisha@example.com>
2019-05-24 09:37:00 +0530

    Lost password html page

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -6,22 +6,26 @@
         <customDropdown :optionList="langList" :default_text="defaut_lang"/>
       </div>
       <div class="signin-form-block">
-        <a href="/home" class="logo-wrap">
+        <a href="/home" class="logo-wrap" title>
           <img src="../assets/images/optimy-logo.png">
         </a>
+        <div class="form-title-block">
+          <h1>Forgot Password</h1>
+          <p>Enter your email address you've using for your account below and we will send you a password reset link</p>
+        </div>
         <b-form class="signin-form">
           <b-form-group>
             <label for>Email Address</label>
-            <b-form-input id type="email" required placeholder="Enter email"></b-form-input>
+            <b-form-input id type="email" required placeholder="Enter your email address..."></b-form-input>
           </b-form-group>
-          <b-form-group>
-            <label for>Password</label>
-            <b-form-input id type="password" required placeholder="Enter Password"></b-form-input>
-          </b-form-group>
-          <b-button type="submit" class="btn-bordersecondary" title="Login">Login</b-button>
+          <b-button
+            type="submit"
+            class="btn btn-bordersecondary"
+            title="Reset my Password"
+          >Reset my Password</b-button>
         </b-form>
         <div class="form-link">
-          <b-link href="/forgotpassword" title="Lost your password">Lost your password?</b-link>
+          <b-link href="/login" title="Login">Login</b-link>
         </div>
       </div>
       <SigninFooter/>
@@ -45,7 +49,11 @@ export default {
       defaut_lang: "EN",
       langList: ["EN", "IND", "FR"]
     };
-  }
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  created() {}
 };
 </script>
 <style lang="scss">
