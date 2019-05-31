1aed89d5c90ff8392c9f296b741b1a60310cef1e
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-05-31 14:08:46 +0530

    Resolved conflicts

--- config/errors.php
+++ config/errors.php
@@ -39,6 +39,9 @@ return [
 		'ERROR_41000' => '41000',
 		'ERROR_40009' => '40009',
 		'ERROR_40010' => '40010',
+		'ERROR_40011' => '40011',
+		'ERROR_40012' => '40012',
+		'ERROR_40013' => '40013',
 		'ERROR_40012' => '40012',
 		'ERROR_40014' => '40014',
 		'ERROR_40016' => '40016',
@@ -54,6 +57,7 @@ return [
 		'ERROR_20014' => '20014',
 		'ERROR_20016' => '20016',
 		'ERROR_20018' => '20018',
+		'ERROR_20022' => '20022',
 		'ERROR_21000' => '21000',
 		'ERROR_20020' => '20020',
 		'ERROR_20022' => '20022',
@@ -76,7 +80,12 @@ return [
 		'41000' => 'Unknown database connection request for this domain',
 		'40009' => 'Domain name is required',
 		'40010' => 'Email address is required',
-		'40012' => 'Token not provided',
+		'40011' => 'Invalid reset password token or email address',
+		'40013' => 'Reset password link is expired or invalid',
+		
+		
+		// Error codes from 2000-
+                '40012' => 'Token not provided',
 		'40014' => 'Provided token is expired',
 		'40016' => 'An error while decoding token',
 		'40018' => 'Database operational error',
