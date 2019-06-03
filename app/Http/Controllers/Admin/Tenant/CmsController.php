88ba7bee4573dad541636cd98472ad29b4b43e18
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-06-03 13:53:26 +0530

    Changed code to App directory

--- app/Http/Controllers/Admin/Tenant/CmsController.php
+++ app/Http/Controllers/Admin/Tenant/CmsController.php
@@ -19,43 +19,7 @@ class CmsController extends Controller
      */
     public function index()
     {
-        try { 
-            // Get data for header table
-            $footerPage = FooterPage::get()->toArray();
-            // dd($footerPage);
-            if (!empty($footerPage)) {
-                foreach ($footerPage as $value) { 
-                    // Get data from footer table                   
-                    $footerPageData = FooterPagesLanguage::where('page_id', $value['page_id'])->get();
-
-                    $detail = array();
-                    $detailArray = array();
-                    foreach ($footerPageData as $languageValue) {
-
-                        $detailArray['language_id'] = $languageValue['language_id'];
-                        $detailArray['title'] = $languageValue['title'];
-                        $detailArray['description'] = $languageValue['description'];
-                        $detail[] = $detailArray;
-                    }
-                    $footerPageList[$value['page_type']] = $detail;
-                }
-
-                $apiData = $footerPageList;
-                $apiStatus = app('Illuminate\Http\Response')->status();
-                $apiMessage = config('messages.success_message.MESSAGE_CMS_LIST_SUCCESS');
-                return Helpers::response($apiStatus, $apiMessage, $apiData);
-            } else {
-                $apiStatus = app('Illuminate\Http\Response')->status();
-                $apiMessage = config('messages.success_message.MESSAGE_NO_DATA_FOUND');
-                return Helpers::response($apiStatus, $apiMessage);
-            }        
-        } catch(\Exception $e) {
-            // Catch database exception
-            return Helpers::errorResponse(config('errors.status_code.HTTP_STATUS_403'), 
-                                        config('errors.status_type.HTTP_STATUS_TYPE_403'), 
-                                        config('errors.custom_error_code.ERROR_40018'), 
-                                        config('errors.custom_error_message.40018'));           
-        }       
+       //
     }
 
     /**
