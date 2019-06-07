ef207abda773d462bfcc77e4a3e9812493dda156
Urvisha <urvisha@example.com>
2019-06-07 15:48:57 +0530

    Landing Page HTML

--- src/components/Footer/SigninFooter.vue
+++ src/components/Footer/SigninFooter.vue
@@ -1,81 +1,81 @@
 <template>
-    <div class="signin-footer">
-        <div class="footer-menu">
-            <b-list-group v-if="isDynamicFooterItemsSet">
-                <b-list-group-item  
-                v-for="item in footerItems" 
-                :to="'/'+getUrl(item)" 
-                :title="getTitle(item)">{{getTitle(item)}}
-                </b-list-group-item>
-            </b-list-group>
-        </div>
-        <div class="copyright-text">
-        <p>{{ $t("label.powered_by") }} <b-link title="Optimy">Optimy</b-link>
-        </p>
-        </div>
+  <div class="signin-footer">
+    <div class="footer-menu">
+      <b-list-group v-if="isDynamicFooterItemsSet">
+        <b-list-group-item
+          v-for="item in footerItems"
+          :to="'/'+getUrl(item)"
+          :title="getTitle(item)"
+        >{{getTitle(item)}}</b-list-group-item>
+      </b-list-group>
     </div>
+    <div class="copyright-text">
+      <p>
+        {{ $t("label.powered_by") }}
+        <b-link title="Optimy">Optimy</b-link>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
-import store from '../../store';
-import {loadLocaleMessages} from '../../services/service';
+import store from "../../store";
+import { loadLocaleMessages } from "../../services/service";
 export default {
-components: {},
+  components: {},
 
-name: "SigninFooter",
+  name: "SigninFooter",
 
-data() {
+  data() {
     return {
-         footerItems: [],
-         isDynamicFooterItemsSet : false
+      footerItems: [],
+      isDynamicFooterItemsSet: false
     };
-},
-mounted() {    
-},
-created() {
-     // Fetching footer CMS pages
-     this.getPageListing();
-     //Fetch language json file
-     loadLocaleMessages(store.state.defaultLanguage)
-},
-methods:{  
-    getPageListing(){
-        axios.get(process.env.VUE_APP_API_ENDPOINT+"cms/listing")
-    .then((response) => {
-
-        if (response.data.data) {
-            this.footerItems = response.data.data
-            this.isDynamicFooterItemsSet = true
-        }
-        }).catch(error => {
-            this.getPageListing();
+  },
+  mounted() {},
+  created() {
+    // Fetching footer CMS pages
+    this.getPageListing();
+    //Fetch language json file
+    loadLocaleMessages(store.state.defaultLanguage);
+  },
+  methods: {
+    getPageListing() {
+      axios
+        .get(process.env.VUE_APP_API_ENDPOINT + "cms/listing")
+        .then(response => {
+          if (response.data.data) {
+            this.footerItems = response.data.data;
+            this.isDynamicFooterItemsSet = true;
+          }
         })
+        .catch(error => {
+          this.getPageListing();
+        });
     },
-    getTitle(items){
-        // console.log(items.pages);return false;
-        //Get title according to language
-        items = items.pages;
-        if (items) { 
-            var filteredObj  = items.filter(function (item,i) { 
-                if (item.language_id == store.state.defaultLanguageId) {
-                    return item;
-                }
-            });
-            if (filteredObj[0]) {
-                return filteredObj[0].title
-            }
+    getTitle(items) {
+      // console.log(items.pages);return false;
+      //Get title according to language
+      items = items.pages;
+      if (items) {
+        var filteredObj = items.filter(function(item, i) {
+          if (item.language_id == store.state.defaultLanguageId) {
+            return item;
+          }
+        });
+        if (filteredObj[0]) {
+          return filteredObj[0].title;
         }
+      }
     },
 
-    getUrl(items){
-        if (items) { 
-            return items.slug
-        }
+    getUrl(items) {
+      if (items) {
+        return items.slug;
+      }
     }
-},
-
+  }
 };
-
 </script>
 
