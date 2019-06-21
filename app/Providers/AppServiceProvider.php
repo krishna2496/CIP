3098d9f4de1d3fb381d6256947b1df202fa5477f
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-06-21 14:09:15 +0530

    added default string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,4 +15,9 @@ class AppServiceProvider extends ServiceProvider
     {
     	// 
     }
+	
+	public function boot() 
+	{
+		Schema::defaultStringLength(191);
+	}
 }
