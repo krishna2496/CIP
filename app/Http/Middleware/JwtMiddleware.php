b5b6b4faa8c24ce6751a6abf43ef08fbfa148750
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-06-24 18:35:52 +0530

    changes of jwt token

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -22,7 +22,7 @@ class JwtMiddleware
 
         if(!$token) {
             // Unauthorized response if token not there
-            return ResponseHelper::error(trans('messages.status_code.HTTP_STATUS_TYPE_401'), 
+            return ResponseHelper::error(trans('messages.status_code.HTTP_STATUS_UNAUTHORIZED'), 
                                         trans('messages.status_type.HTTP_STATUS_TYPE_401'), 
                                         trans('messages.custom_error_code.ERROR_40012'), 
                                         trans('messages.custom_error_message.40012'));
@@ -31,12 +31,12 @@ class JwtMiddleware
 
             $credentials = JWT::decode($token, env('JWT_SECRET'), ['HS256']);
 		} catch(\Firebase\JWT\ExpiredException $e) {
-            return ResponseHelper::error(trans('messages.status_code.HTTP_STATUS_TYPE_401'), 
+            return ResponseHelper::error(trans('messages.status_code.HTTP_STATUS_UNAUTHORIZED'), 
                                         trans('messages.status_type.HTTP_STATUS_TYPE_401'), 
                                         trans('messages.custom_error_code.ERROR_40014'), 
                                         trans('messages.custom_error_message.40014'));
         } catch(\Firebase\JWT\SignatureInvalidException $e) {
-            return ResponseHelper::error(trans('messages.status_code.HTTP_STATUS_TYPE_401'), 
+            return ResponseHelper::error(trans('messages.status_code.HTTP_STATUS_UNAUTHORIZED'), 
                                         trans('messages.status_type.HTTP_STATUS_TYPE_401'), 
                                         trans('messages.custom_error_code.ERROR_40016'), 
                                         trans('messages.custom_error_message.40016'));
