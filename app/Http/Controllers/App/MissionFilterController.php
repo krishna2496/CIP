cd3096a3cb273a49e416bb701f3005f764a0c106
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-06-27 11:12:12 +0530

    user filer changes

---
+++ app/Http/Controllers/App/MissionFilterController.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Http\Controllers\App;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\UserFilter\UserFilterRepository;
+use Illuminate\Http\{Request, Response, JsonResponse};
+use Illuminate\Support\Facades\Input;
+use PDOException;
+use App\Helpers\ResponseHelper;
+
+class UserFilterController extends Controller
+{
+    /**
+     * @var App\Repositories\City\CityRepository 
+     */
+    private $filters;
+    
+    /**
+     * @var Illuminate\Http\Response
+     */
+    private $response;
+    
+    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(UserFilterRepository $filters, Response $response)
+    {
+         $this->filters = $filters;
+         $this->response = $response;
+    }
+    
+    /**
+     * Display listing of footer pages
+     *
+     * @param Illuminate\Http\Request $request
+     * @return mixed
+     */
+    public function index(Request $request):JsonResponse
+    {
+        try {
+            // Get data of user's filter 
+            $filters = $this->filters->userFilter($request);
+
+            if ($filters->count() == 0) {
+                // Set response data
+                $apiStatus = app('Illuminate\Http\Response')->status();
+                $apiMessage = trans('messages.success.MESSAGE_NO_DATA_FOUND');
+                return ResponseHelper::success($apiStatus, $apiMessage);
+            }
+
+            $apiStatus = $this->response->status();
+            $apiMessage = trans('messages.success.MESSAGE_CMS_LIST_SUCCESS');
+            return ResponseHelper::success($apiStatus, $apiMessage,$filters->toArray());
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage());
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+}
