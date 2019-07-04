ff43b8243a6f1360bb1903cc82074d7bb85e4cc9
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-07-04 14:23:49 +0530

    code restructure

--- app/Http/Controllers/App/Mission/MissionController.php
+++ app/Http/Controllers/App/Mission/MissionController.php
@@ -112,9 +112,9 @@ class MissionController extends Controller
     {
         try {
             $languages = $this->languageHelper->getLanguages($request);
-            $local = ($request->hasHeader('X-localization')) ?
+            $language = ($request->hasHeader('X-localization')) ?
             $request->header('X-localization') : env('TENANT_DEFAULT_LANGUAGE_CODE');
-            $language = $languages->where('code', $local)->first();
+            $language = $languages->where('code', $language)->first();
             $languageId = $language->language_id;
             //Save User search data
             $this->userFilterRepository->saveFilter($request);
@@ -172,8 +172,8 @@ class MissionController extends Controller
             );
         } catch (ModelNotFoundException $e) {
             return $this->modelNotFound(
-                config('constants.error_codes.ERROR_NO_DATA_FOUND'),
-                trans('messages.custom_error_message.ERROR_NO_DATA_FOUND')
+                config('constants.error_codes.ERROR_MISSION_NOT_FOUND'),
+                trans('messages.custom_error_message.ERROR_MISSION_NOT_FOUND')
             );
         } catch (PDOException $e) {
             return $this->PDO(
@@ -183,7 +183,7 @@ class MissionController extends Controller
                 )
             );
         } catch (\Exception $e) {
-            throw new \Exception(trans('messages.custom_error_message.ERROR_OCCURED'));
+            throw new \Exception(trans('messages.custom_error_message.ERROR_OCCURRED'));
         }
     }
 
@@ -196,8 +196,8 @@ class MissionController extends Controller
     public function exploreMission(Request $request): JsonResponse
     {
         try {
-            $returnData = [];
-            $local = ($request->hasHeader('X-localization')) ?
+            $apiData = [];
+            $language = ($request->hasHeader('X-localization')) ?
             $request->header('X-localization') : env('TENANT_DEFAULT_LANGUAGE_CODE');
             // Get Data by top theme
             $topTheme = $this->missionRepository->exploreMission($request, config('constants.TOP_THEME'));
@@ -205,41 +205,47 @@ class MissionController extends Controller
             $topCountry = $this->missionRepository->exploreMission($request, config('constants.TOP_COUNTRY'));
             // Get Data by top organization
             $topOrganisation = $this->missionRepository->exploreMission($request, config('constants.TOP_ORGANISATION'));
-            
-            foreach ($topTheme as $key => $value) {
-                if ($value->missionTheme && $value->missionTheme->translations) {
-                    $arrayKey = array_search($local, array_column($value->missionTheme->translations, 'lang'));
+
+            if (!empty($topTheme->toArray())) {
+                foreach ($topTheme as $key => $value) {
+                    if ($value->missionTheme && $value->missionTheme->translations) {
+                        $arrayKey = array_search($language, array_column($value->missionTheme->translations, 'lang'));
                 
-                    if ($arrayKey  !== '') {
-                        $returnData[config('constants.TOP_THEME')][$key]['title'] =
-                        $value->missionTheme->translations[$arrayKey]['title'];
-                        $returnData[config('constants.TOP_THEME')][$key]['id'] =
-                        $value->missionTheme->mission_theme_id;
-                        $returnData[config('constants.TOP_THEME')][$key]['theme_name'] =
-                        $value->missionTheme->theme_name;
+                        if ($arrayKey  !== '') {
+                            $returnData[config('constants.TOP_THEME')][$key]['title'] =
+                            $value->missionTheme->translations[$arrayKey]['title'];
+                                $returnData[config('constants.TOP_THEME')][$key]['id'] =
+                            $value->missionTheme->mission_theme_id;
+                                $returnData[config('constants.TOP_THEME')][$key]['theme_name'] =
+                            $value->missionTheme->theme_name;
+                        }
                     }
                 }
+                $apiData[config('constants.TOP_THEME')] = $returnData[config('constants.TOP_THEME')];
             }
-            foreach ($topCountry as $key => $value) {
-                if ($value->country) {
-                    $returnData[config('constants.TOP_COUNTRY')][$key]['title'] =
-                    $value->country->name;
-                    $returnData[config('constants.TOP_COUNTRY')][$key]['id'] =
-                    $value->country->country_id;
+            if (!empty($topCountry->toArray())) {
+                foreach ($topCountry as $key => $value) {
+                    if ($value->country) {
+                        $returnData[config('constants.TOP_COUNTRY')][$key]['title'] =
+                        $value->country->name;
+                        $returnData[config('constants.TOP_COUNTRY')][$key]['id'] =
+                        $value->country->country_id;
+                    }
                 }
+                $apiData[config('constants.TOP_COUNTRY')] = $returnData[config('constants.TOP_COUNTRY')];
             }
-
-            foreach ($topOrganisation as $key => $value) {
-                if ($value->country) {
-                    $returnData[config('constants.TOP_ORGANISATION')][$key]['title'] =
-                    $value->organisation_name;
-                    $returnData[config('constants.TOP_ORGANISATION')][$key]['id'] =
-                    $value->organisation_id;
+            if (!empty($topOrganisation->toArray())) {
+                foreach ($topOrganisation as $key => $value) {
+                    if ($value->organisation_name != '') {
+                        $returnData[config('constants.TOP_ORGANISATION')][$key]['title'] =
+                        $value->organisation_name;
+                        $returnData[config('constants.TOP_ORGANISATION')][$key]['id'] =
+                        $value->organisation_id;
+                    }
                 }
+                $apiData[config('constants.TOP_ORGANISATION')] = $returnData[config('constants.TOP_ORGANISATION')];
             }
-            $apiData[config('constants.TOP_THEME')] = $returnData[config('constants.TOP_THEME')];
-            $apiData[config('constants.TOP_COUNTRY')] = $returnData[config('constants.TOP_COUNTRY')];
-            $apiData[config('constants.TOP_ORGANISATION')] = $returnData[config('constants.TOP_ORGANISATION')];
+            
             $apiStatus = Response::HTTP_OK;
             return $this->responseHelper->success(
                 $apiStatus,
@@ -248,8 +254,8 @@ class MissionController extends Controller
             );
         } catch (ModelNotFoundException $e) {
             return $this->modelNotFound(
-                config('constants.error_codes.ERROR_NO_DATA_FOUND'),
-                trans('messages.custom_error_message.ERROR_NO_DATA_FOUND')
+                config('constants.error_codes.ERROR_MISSION_NOT_FOUND'),
+                trans('messages.custom_error_message.ERROR_MISSION_NOT_FOUND')
             );
         } catch (PDOException $e) {
             return $this->PDO(
@@ -259,7 +265,7 @@ class MissionController extends Controller
                 )
             );
         } catch (\Exception $e) {
-            throw new \Exception(trans('messages.custom_error_message.ERROR_OCCURED'));
+            throw new \Exception(trans('messages.custom_error_message.ERROR_OCCURRED'));
         }
     }
 }
