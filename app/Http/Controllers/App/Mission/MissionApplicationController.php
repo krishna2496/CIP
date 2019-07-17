93356688d7ced0d936be25e4df9f95cab456e54a
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-07-17 18:25:58 +0530

    renamed variable names

--- app/Http/Controllers/App/Mission/MissionApplicationController.php
+++ app/Http/Controllers/App/Mission/MissionApplicationController.php
@@ -67,11 +67,11 @@ class MissionApplicationController extends Controller
                 );
             }
 
-            $applyCount = $this->missionApplicationRepository->checkApplyMission(
+            $applicationCount = $this->missionApplicationRepository->checkApplyMission(
                 $request->mission_id,
                 $request->auth->user_id
             );
-            if ($applyCount > 0) {
+            if ($applicationCount > 0) {
                 return $this->responseHelper->error(
                     Response::HTTP_UNPROCESSABLE_ENTITY,
                     Response::$statusTexts[Response::HTTP_UNPROCESSABLE_ENTITY],
@@ -80,8 +80,8 @@ class MissionApplicationController extends Controller
                 );
             }
 
-            $available = $this->missionApplicationRepository->checkAvailableSeats($request->mission_id);
-            if ($available == false) {
+            $seatAvailable = $this->missionApplicationRepository->checkAvailableSeats($request->mission_id);
+            if (!$seatAvailable) {
                 return $this->responseHelper->error(
                     Response::HTTP_UNPROCESSABLE_ENTITY,
                     Response::$statusTexts[Response::HTTP_UNPROCESSABLE_ENTITY],
@@ -90,8 +90,8 @@ class MissionApplicationController extends Controller
                 );
             }
 
-            $deadline = $this->missionApplicationRepository->checkMissionDeadline($request->mission_id);
-            if ($deadline == false) {
+            $applicationDeadline = $this->missionApplicationRepository->checkMissionDeadline($request->mission_id);
+            if (!$applicationDeadline) {
                 return $this->responseHelper->error(
                     Response::HTTP_UNPROCESSABLE_ENTITY,
                     Response::$statusTexts[Response::HTTP_UNPROCESSABLE_ENTITY],
