c68f6992c13a95cf5fd72811f420dacfa27f9448
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-07-22 10:51:38 +0530

    translations added

--- resources/lang/fr/mail.php
+++ resources/lang/fr/mail.php
@@ -5,9 +5,9 @@ return [
     * Recommonded mission
     */
     'recommonded_mission' => [
-        'HAS_RECOMMENDED_A_MISSION_TO_YOU' => 'FR: has recommended a mission to you',
-        'MISSION' => 'FR: MISSION :',
-        'ALL_RIGHTS_RESERVED' => 'FR: All Rights Reserved.',
-        'MAIL_MISSION_RECOMMENDATION' => 'FR: Mission Recommendation'
+        'HAS_RECOMMENDED_A_MISSION_TO_YOU' => 'vous a recommandé une mission',
+        'MISSION' => 'MISSION :',
+        'ALL_RIGHTS_RESERVED' => 'Tous les droits sont réservés.',
+        'MAIL_MISSION_RECOMMENDATION' => 'Recommandation de mission'
     ]
 ];
