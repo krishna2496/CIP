29bb6ed0423ff85ede4af851741a1ddada8eba17
SZ <siddharajsinh.zala@sparsh-technologies.co.in>
2019-07-24 18:44:49 +0530

    Remove css from index.html and replace svg code with image on landing page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,28 +51,9 @@
                 <!-- grid view -->
                     <b-tab class="grid-tab-content">
                         <template slot="title">
-                        <i class="grid" @click="activeView = 'gridView'">
-                            <svg
-                            version="1.1"
-                            id="Capa_1"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink"
-                            x="0px"
-                            y="0px"
-                            viewBox="0 0 174.239 174.239"
-                            style="enable-background:new 0 0 174.239 174.239;"
-                            xml:space="preserve"
-                            >
-                            <g><g>
-                            <path d="M174.239,174.239H96.945V96.945h77.294V174.239z M111.88,159.305h47.425V111.88H111.88V159.305z"></path>
-                            </g><g>
-                            <path d="M77.294,174.239H0V96.945h77.294V174.239z M14.935,159.305H62.36V111.88H14.935V159.305z"></path>
-                            </g><g>
-                            <path d="M174.239,77.294H96.945V0h77.294V77.294z M111.88,62.36h47.425V14.935H111.88V62.36z"></path>
-                            </g><g>
-                            <path d="M77.294,77.294H0V0h77.294V77.294z M14.935,62.36H62.36V14.935H14.935V62.36z"></path>
-                            </g></g>
-                            </svg>
+                        <i class="grid" @click="activeView = 'gridView'" v-b-tooltip.hover title="Grid View">
+                             <img class="img-normal" :src="$store.state.imagePath+'/assets/images/grid.svg'" alt="Down Arrow" />
+                             <img class="img-rollover" :src="$store.state.imagePath+'/assets/images/grid-h.svg'" alt="Down Arrow" />
                         </i>
                         </template>
                         <GridView 
@@ -89,15 +70,9 @@
                         <!-- list view -->
                     <b-tab class="list-tab-content">
                         <template slot="title">
-                        <i class="list" @click="activeView = 'listView'">
-                            <svg
-                            id="Layer_1"
-                            data-name="Layer 1"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 22 22"
-                            >
-                            <path id="List" class="cls-1" d="M0,0H22V2H0ZM0,10H22v2H0ZM0,20H22v2H0Z"></path>
-                            </svg>
+                        <i class="list" @click="activeView = 'listView'" v-b-tooltip.hover title="List View">
+                            <img class="img-normal" :src="$store.state.imagePath+'/assets/images/list.svg'" alt="Down Arrow" />
+                            <img class="img-rollover" :src="$store.state.imagePath+'/assets/images/list-h.svg'" alt="Down Arrow" />
                         </i>
                         </template>
                         <ListView
@@ -132,27 +107,9 @@
             <TheSecondaryFooter></TheSecondaryFooter>
         </footer>
         <back-to-top bottom="68px" right="40px" :title="$t('label.back_to_top')">
-        <i>
-            <svg
-                version="1.1"
-                id="Capa_1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                width="451.847px"
-                height="451.847px"
-                viewBox="0 0 451.847 451.847"
-                style="enable-background:new 0 0 451.847 451.847;"
-                xml:space="preserve">
-            <g>
-            <path
-            d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751
-            c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0
-            c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z"
-            ></path>
-            </g>
-            </svg>
+         <i class="icon-wrap">
+            <img class="img-normal" :src="$store.state.imagePath+'/assets/images/down-arrow.svg'" alt="Down Arrow" />
+            <img class="img-rollover" :src="$store.state.imagePath+'/assets/images/down-arrow-black.svg'" alt="Down Arrow" />
         </i>
         </back-to-top>
     </div>
