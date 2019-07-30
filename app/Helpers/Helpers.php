a559b8ca763dcaef13964f22114de14a8505d8e6
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-07-30 19:04:26 +0530

    code restructure

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -269,7 +269,7 @@ class Helpers
     }
 
     /**
-     * Get user token for testing
+     * Get JWT token
      *
      * @param int $userId
      * @return string
@@ -277,10 +277,10 @@ class Helpers
     public static function getJwtToken(int $userId) : string
     {
         $payload = [
-            'iss' => "lumen-jwt",
-            'sub' => $userId,
-            'iat' => time(),
-            'exp' => time() + 60 * 60,
+            'iss' => "lumen-jwt", // Issuer of the token
+            'sub' => $userId, // Subject of the token
+            'iat' => time(), // Time when JWT was issued.
+            'exp' => time() + 60 * 60, // Expiration time
             'fqdn' => env('DEFAULT_TENANT')
         ];
         // As you can see we are passing `JWT_SECRET` as the second parameter that will
--- app/Http/Controllers/App/Auth/AuthController.php
+++ app/Http/Controllers/App/Auth/AuthController.php
@@ -82,27 +82,6 @@ class AuthController extends Controller
     }
 
     /**
-     * Create a new token.
-     *
-     * @param \App\User $user
-     * @return string
-     */
-    protected function jwt(User $user) :String
-    {
-        $payload = [
-            'iss' => "lumen-jwt",       // Issuer of the token
-            'sub' => $user->user_id,    // Subject of the token
-            'iat' => time(),            // Time when JWT was issued.
-            'exp' => time() + 60 * 60,  // Expiration time
-            'fqdn' => 'tatva'
-        ];
-
-        // As you can see we are passing `JWT_SECRET` as the second parameter that will
-        // be used to decode the token in the future.
-        return JWT::encode($payload, env('JWT_SECRET'));
-    }
-
-    /**
      * Authenticate a user and return the token if the provided credentials are correct.
      *
      * @param \App\User $user
