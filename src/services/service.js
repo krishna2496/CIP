b39e6c34fef0246130ddb1543aaa488597d545f0
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-07-30 10:25:13 +0530

    added comma

--- src/services/service.js
+++ src/services/service.js
@@ -17,7 +17,7 @@ import applyMission from "./Mission/ApplyMission";
 import storeMissionRating from "./Mission/StoreMissionRating";
 import missionVolunteers from "./Mission/MissionVolunteers";
 import missionCarousel from "./Mission/MissionCarousel";
-import missionDetail from "./Mission/MissionDetail"
+import missionDetail from "./Mission/MissionDetail";
 export {
     loadLocaleMessages,
     missionListing,
@@ -37,6 +37,6 @@ export {
     applyMission,
     storeMissionRating,
     missionVolunteers,
-	missionCarousel
+	missionCarousel,
     missionDetail
 }
