77940d37b8272a2187b0d1ea53f3e6b07880eaeb
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-08-12 11:51:02 +0530

    Added testcases for change password

--- tests/App/AppUserTest.php
+++ tests/App/AppUserTest.php
@@ -98,4 +98,145 @@ class AppUserTest extends TestCase
         ]);
         $user->delete();
     }
+
+    /**
+     * @test
+     *
+     * Change password
+     *
+     * @return void
+     */
+    public function it_should_change_password()
+    {
+        $connection = 'tenant';
+        $user = factory(\App\User::class)->make();
+        $user->setConnection($connection);
+        $user->save();
+        $user->password = "123456789";
+        $user->update();
+
+        $params = [
+            'old_password' => "123456789",
+            'password' => "12345678",
+            'confirm_password' => "12345678"
+        ];
+        $token = Helpers::getJwtToken($user->user_id);
+        $this->patch('app/change-password', $params, ['token' => $token])
+        ->seeStatusCode(200)
+        ->seeJsonStructure(
+            [
+            "status",
+            "data" =>[
+                "token"
+            ],
+            "message"
+            ]
+        );
+        $user->delete();
+    }
+
+    /**
+     * @test
+     *
+     * Show error if incorrect old password
+     *
+     * @return void
+     */
+    public function it_should_show_error_for_incorrect_old_password()
+    {
+        $connection = 'tenant';
+        $user = factory(\App\User::class)->make();
+        $user->setConnection($connection);
+        $user->save();
+
+        $params = [
+            'old_password' => "test",
+            'password' => "12345678",
+            'confirm_password' => "12345678"
+        ];
+        $token = Helpers::getJwtToken($user->user_id);
+        $this->patch('app/change-password', $params, ['token' => $token])
+        ->seeStatusCode(422)
+        ->seeJsonStructure([
+            'errors' => [
+                [
+                    'status',
+                    'type',
+                    'code',
+                    'message'
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @test
+     *
+     * Show error if password and confirm password does not matched
+     *
+     * @return void
+     */
+    public function it_should_show_error_for_new_password_does_not_matched()
+    {
+        $connection = 'tenant';
+        $user = factory(\App\User::class)->make();
+        $user->setConnection($connection);
+        $user->save();
+        $user->password = "123456789";
+        $user->update();
+
+        $params = [
+            'old_password' => "123456789",
+            'password' => "12345678",
+            'confirm_password' => "1234567800"
+        ];
+        $token = Helpers::getJwtToken($user->user_id);
+        $this->patch('app/change-password', $params, ['token' => $token])
+        ->seeStatusCode(422)
+        ->seeJsonStructure([
+            'errors' => [
+                [
+                    'status',
+                    'type',
+                    'code',
+                    'message'
+                ]
+            ]
+        ]);
+        $user->delete();
+    }
+
+    /**
+     * @test
+     *
+     * Show error if required fields are empty
+     *
+     * @return void
+     */
+    public function it_should_show_error_if_required_fields_are_empty_for_chamhe_password()
+    {
+        $connection = 'tenant';
+        $user = factory(\App\User::class)->make();
+        $user->setConnection($connection);
+        $user->save();
+
+        $params = [
+            'old_password' => "",
+            'password' => "",
+            'confirm_password' => ""
+        ];
+        $token = Helpers::getJwtToken($user->user_id);
+        $this->patch('app/change-password', $params, ['token' => $token])
+        ->seeStatusCode(422)
+        ->seeJsonStructure([
+            'errors' => [
+                [
+                    'status',
+                    'type',
+                    'code',
+                    'message'
+                ]
+            ]
+        ]);
+    }
 }
