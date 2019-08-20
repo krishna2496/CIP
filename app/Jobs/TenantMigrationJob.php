581d8a48c1ace6336d99f265d47f1614fa3db182
SZ <siddharajsinh.zala@sparsh-technologies.co.in>
2019-08-20 17:55:52 +0530

    Notification email function changed

--- app/Jobs/TenantMigrationJob.php
+++ app/Jobs/TenantMigrationJob.php
@@ -22,6 +22,18 @@ class TenantMigrationJob extends Job
      * @var App\Helpers\DatabaseHelper
      */
     protected $databaseHelper;
+
+    /**
+     * @var string
+     */
+    private $emailMessage;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 1;
     
     /**
      * Create a new job instance.
@@ -33,6 +45,7 @@ class TenantMigrationJob extends Job
     {
         $this->tenant = $tenant;
         $this->databaseHelper = new DatabaseHelper;
+        $this->emailMessage = 'Job passed successufully';
     }
 
     /**
@@ -42,84 +55,69 @@ class TenantMigrationJob extends Job
      */
     public function handle()
     {
-        // Job will try to attempt only one time. If need to re-attempt then it will delete job from table
-        if ($this->attempts() < 2) {
-            // do job things
-            try {
-                // Create database
-                DB::statement("CREATE DATABASE IF NOT EXISTS `ci_tenant_{$this->tenant->tenant_id}`");
+        try {
+            // Create database
+            DB::statement("CREATE DATABASE IF NOT EXISTS `ci_tenant_{$this->tenant->tenant_id}`");
 
-                // Connect with newly created database
-                $this->databaseHelper->connectWithTenantDatabase($this->tenant->tenant_id);
-                
-                // Call artisan command to create table for newly created tenant's database
-                Artisan::call('migrate --path=database/migrations/tenant');
-
-                // Call artisan command to run database seeder for default values
-                Artisan::call('db:seed');
-                
-                // Disconnect and reconnect with default database
-                DB::disconnect('tenant');
-                DB::reconnect('mysql');
-                DB::setDefaultConnection('mysql');
-
-                // Send success mail to super admin
-                $message = "<p> Tenant : " .$this->tenant->name. "<br>";
-                $message .= "Background Job Name : Tenant Migration Job <br>";
-                $message .= "Background Job Status : Success <br>";
-
-                $this->sendEmailNotification($message);
-
-                Log::info($message);
-            } catch (\Exception $e) {
-                $message = "<p> Tenant : " .$this->tenant->name. "<br>";
-                $message .= "Background Job Name : Tenant Migration Job <br>";
-                $message .= "Background Job Status : Failed <br>";
-                $message .= "Message : Tenant and database have been deleted. </p>";
-    
-                $this->sendEmailNotification($message, true);
-    
-                Log::info($message);
-                                
-                // Delete created tenant
-                $this->tenant->delete();
-                // Drop tanant database
-                DB::statement("DROP DATABASE IF EXISTS `ci_tenant_{$this->tenant->tenant_id}`");
-            }
-        } else {
-            $message = "<p> Tenant : " .$this->tenant->name. "<br>";
-            $message .= "Background Job Name : Tenant Migration Job <br>";
-            $message .= "Background Job Status : Failed <br>";
-            $message .= "Message : Job deleted from database. </p>";
-
-            $this->sendEmailNotification($message, true);
-
-            Log::info($message);
+            // Connect with newly created database
+            $this->databaseHelper->connectWithTenantDatabase($this->tenant->tenant_id);
             
-            // Delete job from database
-            $this->delete();
+            // Call artisan command to create table for newly created tenant's database
+            Artisan::call('migrate --path=database/migrations/tenant');
+
+            // Call artisan command to run database seeder for default values
+            Artisan::call('db:seed');
+            
+            // Disconnect and reconnect with default database
+            DB::disconnect('tenant');
+            DB::reconnect('mysql');
+            DB::setDefaultConnection('mysql');
+
+            // Send success mail to super admin
+            $this->sendEmailNotification();
+        } catch (\Exception $e) {
+            $this->emailMessage = 'Error while creating migration for tenant';
+            $this->sendEmailNotification(true);
+            $this->tenant->delete();
+            DB::statement("DROP DATABASE IF EXISTS `ci_tenant_{$this->tenant->tenant_id}`");
         }
     }
 
     /**
      * Send email notification to admin
-     * @param string $message
      * @param bool $isFail
      * @return void
      */
-    public function sendEmailNotification(string $message, bool $isFail = false)
+    public function sendEmailNotification(bool $isFail = false)
     {
+        $status = ($isFail) ? 'Failed' : 'Passed';
+
+        $message = "<p> Tenant : " .$this->tenant->name. "<br>";
+        $message .= "Background Job Name : Tenant Migration <br>";
+        $message .= "Background Job Status : ".$status." <br>";
+
         $data = array(
             'message'=> $message,
             'tenant_name' => $this->tenant->name
         );
-        
+
         $params['to'] = config('constants.ADMIN_EMAIL_ADDRESS'); //required
         $params['template'] = config('constants.EMAIL_TEMPLATE_FOLDER').'.'.config('constants.EMAIL_TEMPLATE_USER_INVITE'); //path to the email template
-        $params['subject'] = ($isFail) ? 'Error in tenant creation : '. $this->tenant->name :
-        'Success tenant migration job : '.$this->tenant->name; //optional
+        $params['subject'] = ($isFail) ? 'Error: Tenant Migration Job For '.$this->tenant->name. ' Tenant' :
+        'Success: Tenant Migration Job For '.$this->tenant->name. ' Tenant'; //optional
         $params['data'] = $data;
 
         $this->sendEmail($params);
+    }
+
+    /**
+     * The job failed to process.
+     *
+     * @param  Exception  $exception
+     * @return void
+     */
+    public function failed(\Exception $exception)
+    {
+        $this->sendEmailNotification(true);
     }
 }
