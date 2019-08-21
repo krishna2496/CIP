9ea15655b46e438d0223cd61ead7b23444ec098d
Krishna Dhanani <krishna.dhanani@sparsh1.com>
2019-08-21 10:18:57 +0530

    plugin removed

--- src/router.js
+++ src/router.js
@@ -9,12 +9,6 @@ let routes = [{
     path: '*',
     redirect: '/404'
 },
-{
-    path: '/picture',
-    name: 'picture',
-    component: () =>
-        import ('./views/picture.vue')
-},
 { 
     path: '/404',
     name: '404',
