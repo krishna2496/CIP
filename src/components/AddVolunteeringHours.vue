b3bdb15b484d4f4a7ec6607b121ba64b0607951a
Krishna Dhanani <krishna.dhanani@sparsh1.com>
2019-09-24 17:47:21 +0530

    hour validation

--- src/components/AddVolunteeringHours.vue
+++ src/components/AddVolunteeringHours.vue
@@ -241,69 +241,7 @@
                 classVariant: "success",
                 isFileUploadDisplay: false,
                 isAjaxCall: false,
-                minuteList: [
-                    ["00", "00"],
-                    ["01", "01"],
-                    ["02", "02"],
-                    ["03", "03"],
-                    ["04", "04"],
-                    ["05", "05"],
-                    ["06", "06"],
-                    ["07", "07"],
-                    ["08", "08"],
-                    ["09", "09"],
-                    ["10", "10"],
-                    ["11", "11"],
-                    ["12", "12"],
-                    ["13", "13"],
-                    ["14", "14"],
-                    ["15", "15"],
-                    ["16", "16"],
-                    ["17", "17"],
-                    ["18", "18"],
-                    ["19", "19"],
-                    ["20", "20"],
-                    ["21", "21"],
-                    ["22", "22"],
-                    ["23", "23"],
-                    ["24", "20"],
-                    ["25", "2"],
-                    ["26", "22"],
-                    ["27", "27"],
-                    ["28", "28"],
-                    ["29", "29"],
-                    ["30", "30"],
-                    ["31", "31"],
-                    ["32", "32"],
-                    ["33", "33"],
-                    ["34", "34"],
-                    ["35", "35"],
-                    ["36", "36"],
-                    ["37", "37"],
-                    ["38", "38"],
-                    ["39", "39"],
-                    ["40", "40"],
-                    ["41", "41"],
-                    ["42", "42"],
-                    ["43", "43"],
-                    ["44", "44"],
-                    ["45", "45"],
-                    ["46", "46"],
-                    ["47", "47"],
-                    ["48", "48"],
-                    ["49", "49"],
-                    ["50", "50"],
-                    ["51", "51"],
-                    ["52", "52"],
-                    ["53", "53"],
-                    ["54", "50"],
-                    ["55", "51"],
-                    ["56", "52"],
-                    ["57", "57"],
-                    ["58", "58"],
-                    ["59", "59"]
 
-                ],
                 saveVolunteerHours: {
                     mission_id: "",
                     date_volunteered: "",
@@ -316,15 +254,6 @@
             }
         },
         validations() {
-
-            // const requiredHourValidation = (
-            //     (this.timeEntryDefaultData.minutes == '' || this.timeEntryDefaultData.hours == '') ||
-            //     (this.timeEntryDefaultData.minutes == '00' && this.timeEntryDefaultData.hours == '00') ||
-            //     (this.timeEntryDefaultData.minutes == '00' && this.timeEntryDefaultData.hours == '') ||
-            //     (this.timeEntryDefaultData.minutes == '' && this.timeEntryDefaultData.hours == '00')
-            // ) ? {
-            //     required
-            // } : {};
 
             const requiredHourValidation = (
                 (this.timeEntryDefaultData.hours == '') ||
@@ -372,7 +301,7 @@
             },
             minuteChange() {
                 if(this.timeEntryDefaultData.minutes == "00") {
-                    this.timeEntryDefaultData.minutes = Math.floor(this.timeEntryDefaultData.minutes)
+                    this.timeEntryDefaultData.minutes = Math.floor(this.timeEntryDefaultData.minutes).toString()
                 }
             },
             dateChange() {
