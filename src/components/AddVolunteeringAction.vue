1864b324d8e34ce724a98e301a63790faeb56e3f
Krishna Dhanani <krishna.dhanani@sparsh1.com>
2019-09-25 10:42:58 +0530

    file upload validation changes

--- src/components/AddVolunteeringAction.vue
+++ src/components/AddVolunteeringAction.vue
@@ -211,95 +211,6 @@
                 message: null,
                 classVariant: "success",
                 fileError: "",
-                hourList: [
-                    ["00", "00"],
-                    ["01", "01"],
-                    ["02", "02"],
-                    ["03", "03"],
-                    ["04", "04"],
-                    ["05", "05"],
-                    ["06", "06"],
-                    ["07", "07"],
-                    ["08", "08"],
-                    ["09", "09"],
-                    ["10", "10"],
-                    ["11", "11"],
-                    ["12", "12"],
-                    ["13", "13"],
-                    ["14", "14"],
-                    ["15", "15"],
-                    ["16", "16"],
-                    ["17", "17"],
-                    ["18", "18"],
-                    ["19", "19"],
-                    ["20", "20"],
-                    ["21", "21"],
-                    ["22", "22"],
-                    ["23", "23"]
-                ],
-                minuteList: [
-                    ["00", "00"],
-                    ["01", "01"],
-                    ["02", "02"],
-                    ["03", "03"],
-                    ["04", "04"],
-                    ["05", "05"],
-                    ["06", "06"],
-                    ["07", "07"],
-                    ["08", "08"],
-                    ["09", "09"],
-                    ["10", "10"],
-                    ["11", "11"],
-                    ["12", "12"],
-                    ["13", "13"],
-                    ["14", "14"],
-                    ["15", "15"],
-                    ["16", "16"],
-                    ["17", "17"],
-                    ["18", "18"],
-                    ["19", "19"],
-                    ["20", "20"],
-                    ["21", "21"],
-                    ["22", "22"],
-                    ["23", "23"],
-                    ["24", "20"],
-                    ["25", "2"],
-                    ["26", "22"],
-                    ["27", "27"],
-                    ["28", "28"],
-                    ["29", "29"],
-                    ["30", "30"],
-                    ["31", "31"],
-                    ["32", "32"],
-                    ["33", "33"],
-                    ["34", "34"],
-                    ["35", "35"],
-                    ["36", "36"],
-                    ["37", "37"],
-                    ["38", "38"],
-                    ["39", "39"],
-                    ["40", "40"],
-                    ["41", "41"],
-                    ["42", "42"],
-                    ["43", "43"],
-                    ["44", "44"],
-                    ["45", "45"],
-                    ["46", "46"],
-                    ["47", "47"],
-                    ["48", "48"],
-                    ["49", "49"],
-                    ["50", "50"],
-                    ["51", "51"],
-                    ["52", "52"],
-                    ["53", "53"],
-                    ["54", "50"],
-                    ["55", "51"],
-                    ["56", "52"],
-                    ["57", "57"],
-                    ["58", "58"],
-                    ["59", "59"]
-
-                ],
                 saveVolunteerHours: {
                     mission_id: "",
                     date_volunteered: "",
@@ -338,6 +249,8 @@
                 var _this = this
                 let allowedFileTypes = ['doc', 'xls', 'xlsx', 'csv', 'pdf', 'png', 'jpg', 'jpeg']
                 _this.fileError = '';
+                let error = false
+                let duplicateUpload = false
                 let latestUpload = files[files.length - 1];
                 let latestUploadName = latestUpload.name
                 let latestUploadSize = latestUpload.size
@@ -347,19 +260,27 @@
                     let fileName = data.name.split('.');
                     if (!allowedFileTypes.includes(fileName[fileName.length - 1])) {
                         _this.fileError = _this.langauageData.errors.invalid_file_type
-                        files.splice(index, 1)
+                        error = true
                     } else {
                         if (data.size > 4000000) {
                             _this.fileError = _this.langauageData.errors.file_max_size
-                            files.splice(index, 1)
+                          
+                            error = true
                         }
                     }
                     if (index != files.length - 1) {
                         if (data.name == latestUploadName && data.size == latestUploadSize && data.type ==
                             latestUploadType) {
-
                             _this.fileError = _this.langauageData.errors.file_already_uploaded
+                            error = true
+                            duplicateUpload = true;
+                        }
+                    }
+                    if(error == true) {
+                        if(duplicateUpload == true) {
                             files.splice(files.length - 1, 1)
+                        } else {
+                            files.splice(index, 1)
                         }
                     }
                 });
