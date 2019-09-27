f96f9827ee4de577f13215c20fe472a5ce6e3080
Siddharajsinh Zala <siddharajsinh.zala@tatvasoft.com>
2019-09-27 11:30:16 +0530

    Custom css apply on page load

--- src/services/CustomCss.js
+++ src/services/CustomCss.js
@@ -1,9 +1,15 @@
-import axios from 'axios'
+import axios from "axios";
+
 export default async () => {
-        var apiUrl = process.env.VUE_APP_API_ENDPOINT
-        var getDynamicCssUrl = apiUrl + 'app/custom-css';        
-        await axios.get(getDynamicCssUrl).then( response => {
-            document.getElementById('customCss').setAttribute("href", response.data.data.custom_css);            
-        })
-       
-}
+  console.log("before axios");
+  var apiUrl = process.env.VUE_APP_API_ENDPOINT;
+  var getDynamicCssUrl = apiUrl + "app/custom-css";
+  await axios.get(getDynamicCssUrl).then(response => {
+    document
+      .getElementById("customCss")
+	  .setAttribute("href", response.data.data.custom_css);
+	  return new Promise(resolve => {
+		setTimeout(() => resolve('I did something'), 1000)
+	  })    
+  });
+};
