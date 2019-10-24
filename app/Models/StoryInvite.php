7a3cef03a47f9568ebbdf2e040bd6dc482d26db6
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-10-24 17:31:36 +0530

    Changed query

--- app/Models/StoryInvite.php
+++ app/Models/StoryInvite.php
@@ -55,7 +55,7 @@ class StoryInvite extends Model
      */
     public function getDetails(int $inviteId): StoryInvite
     {
-        return $this->with(['toUser', 'fromUser', 'story'])->where('mission_invite_id', $inviteId)->first();
+        return $this->with(['toUser', 'fromUser', 'story'])->where('story_invite_id', $inviteId)->first();
     }
 
     /**
