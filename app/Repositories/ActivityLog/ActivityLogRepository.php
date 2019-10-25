988106ba11aad0fdd4957174e665dce70b093a2f
SZ <siddharajsinh.zala@sparsh-technologies.co.in>
2019-10-25 11:27:43 +0530

    Fetch activity log api

--- app/Repositories/ActivityLog/ActivityLogRepository.php
+++ app/Repositories/ActivityLog/ActivityLogRepository.php
@@ -4,6 +4,8 @@ namespace App\Repositories\ActivityLog;
 use App\Models\ActivityLog;
 use App\Repositories\ActivityLog\ActivityLogInterface;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use DB;
 
 class ActivityLogRepository implements ActivityLogInterface
 {
@@ -32,5 +34,33 @@ class ActivityLogRepository implements ActivityLogInterface
     public function storeActivityLog(array $data)
     {
         $this->activityLog->create($data);
+    }
+
+    /**
+     * Display a listing of specified resources with pagination.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Pagination\LengthAwarePaginator
+     */
+    public function getActivityLogs(Request $request): LengthAwarePaginator
+    {
+        $type = $request->type;
+        $action = $request->action;
+        $order = !empty($request->order) ? $request->order : 'desc';
+        $fromDate = $request->from_date;
+        $toDate = $request->to_date;
+
+        $activityLogQuery = $this->activityLog
+            ->when($type, function ($query, $type) {
+                return $query->where('type', $type);
+            })->when($action, function ($query, $action) {
+                return $query->where('action', $action);
+            });
+
+        if (!empty($fromDate) && !empty($toDate)) {
+            $activityLogQuery->whereBetween(DB::raw('DATE(created_at)'), [$fromDate, $toDate]);
+        }
+
+        return $activityLogQuery->orderBy('created_at', $order)->paginate($request->perPage);
     }
 }
