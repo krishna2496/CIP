c37fef62e04f31eb971cbeace0f44246cff60940
Siddharajsinh Zala <siddharajsinh.zala@sparsh-technologies.co.in>
2019-11-14 19:12:01 +0530

    Code restructure

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,8 +45,7 @@ class Handler extends ExceptionHandler
      * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
     public function render($request, Exception $exception)
-    {
-        dd($exception);
+    {        
         return $this->internalServerError();
     }
 }
