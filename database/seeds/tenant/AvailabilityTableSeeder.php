574c13d4798ebbf1e4876ac86c29b8a7dd995d68
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-11-18 17:54:33 +0530

    Changed migrations

--- database/seeds/tenant/AvailabilityTableSeeder.php
+++ database/seeds/tenant/AvailabilityTableSeeder.php
@@ -15,16 +15,19 @@ class AvailabilityTableSeeder extends Seeder
         $items = [            
             [                
                 "type" => "Anytime",
+                "translations" => 'a:2:{i:0;a:2:{s:4:"lang";s:2:"en";s:5:"title";s:7:"Anytime";}i:1;a:2:{s:4:"lang";s:2:"fr";s:5:"title";s:10:"Anytime fr";}}',
                 "created_at" => Carbon::now(),
                 "updated_at" => Carbon::now()
             ],
             [
                 "type" => "Weekend only",
+                "translations" => 'a:1:{i:0;a:2:{s:4:"lang";s:2:"en";s:5:"title";s:12:"Weekend only";}}',
                 "created_at" => Carbon::now(),
                 "updated_at" => Carbon::now()
             ],
             [
                 "type" => "Work Week only",
+                "translations" => 'a:1:{i:0;a:2:{s:4:"lang";s:2:"en";s:5:"title";s:14:"Work Week only";}}',
                 "created_at" => Carbon::now(),
                 "updated_at" => Carbon::now()
             ]
