198562a2990282a2b390c51b0cae95c7c76415e1
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-12-05 11:47:29 +0530

    Added migrations for sort_order

---
+++ database/migrations/tenant/2019_12_05_045704_add_column_sort_order_for_mission_media_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnSortOrderForMissionMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::getDoctrineSchemaManager()
+                ->getDatabasePlatform()
+                ->registerDoctrineTypeMapping('enum', 'string');
+        Schema::table('mission_media', function (Blueprint $table) {
+            $table->integer('sort_order')->length(11)->default(0)->after('default');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mission_media', function (Blueprint $table) {
+            $table->dropColumn('sort_order');
+        });
+    }
+}
