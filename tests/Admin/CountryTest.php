58e72d225794e2c1434396537082afbfa6e74fe9
Siddharajsinh Zala <siddharajsinh.zala@tatvasoft.com>
2019-12-16 10:26:01 +0530

    Country test cases

--- tests/Admin/CountryTest.php
+++ tests/Admin/CountryTest.php
@@ -531,4 +531,177 @@ class CountryTest extends TestCase
         $this->delete("countries/".rand(1000000, 5000000), [], ['Authorization' => 'Basic '.base64_encode(env('API_KEY').':'.env('API_SECRET'))])
         ->seeStatusCode(404);
     }
+    
+    /**
+     * @test
+     */
+    public function it_should_return_validation_error_on_iso_exist_on_add_country()
+    {
+        // Get all languages
+        DB::setDefaultConnection('mysql');
+
+        $tenantLanguage = DB::table('tenant_language')
+        ->join('language', 'tenant_language.language_id', 'language.language_id')
+        ->where('tenant_id', env('DEFAULT_TENANT_ID'))
+        ->inRandomOrder()
+        ->first();
+
+        if (is_null($tenantLanguage)) {
+            $randomLangage = DB::table('language')->inRandomOrder()->first();
+            
+            $tenantLanguageId = DB::table('tenant_language')->insertGetId([
+                'tenant_id' => env('DEFAULT_TENANT_ID'),
+                'language_id' => $randomLangage->language_id,
+                'default' => '1'
+            ]);
+            
+            $tenantLanguage = DB::table('tenant_language')
+            ->join('language', 'tenant_language.language_id', 'language.language_id')
+            ->where('tenant_id', env('DEFAULT_TENANT_ID'))
+            ->inRandomOrder()
+            ->first();
+        }
+        
+        $countryISO = str_random(2);
+        
+        // Get random langauge for country name
+        $params = [
+            "countries" => [
+                [
+                    "iso" => $countryISO,
+                    "translations"=> [
+                        [
+                            "lang"=> $tenantLanguage->code,
+                            "name"=> str_random(5)
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $response = $this->post("countries", $params, ['Authorization' => 'Basic '.base64_encode(env('API_KEY').':'.env('API_SECRET'))])
+        ->seeStatusCode(201);
+
+        $countryId = json_decode($response->response->getContent())->data->country_ids[0]->country_id;
+        
+        DB::setDefaultConnection('mysql');
+
+        // Add another country with same ISO code        
+        $params = [
+            "countries" => [
+                [
+                    "iso" => $countryISO,
+                    "translations"=> [
+                        [
+                            "lang"=> $tenantLanguage->code,
+                            "name"=> str_random(5)
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        
+        $response = $this->post("countries", $params, ['Authorization' => 'Basic '.base64_encode(env('API_KEY').':'.env('API_SECRET'))])
+        ->seeStatusCode(422);
+
+        DB::setDefaultConnection('mysql');
+
+        // Delete country and country_language data
+        $this->delete("countries/$countryId", [], ['Authorization' => 'Basic '.base64_encode(env('API_KEY').':'.env('API_SECRET'))])
+        ->seeStatusCode(204);
+
+        // Delete tenant language
+        DB::setDefaultConnection('mysql');
+        
+        DB::table('tenant_language')
+        ->where('tenant_language_id', $tenantLanguage->tenant_language_id)
+        ->delete();
+    } 
+
+    /**
+     * @test
+     */
+    public function it_should_return_validation_error_on_country_name_exist_on_add_country()
+    {
+        // Get all languages
+        DB::setDefaultConnection('mysql');
+
+        $tenantLanguage = DB::table('tenant_language')
+        ->join('language', 'tenant_language.language_id', 'language.language_id')
+        ->where('tenant_id', env('DEFAULT_TENANT_ID'))
+        ->inRandomOrder()
+        ->first();
+
+        if (is_null($tenantLanguage)) {
+            $randomLangage = DB::table('language')->inRandomOrder()->first();
+            
+            $tenantLanguageId = DB::table('tenant_language')->insertGetId([
+                'tenant_id' => env('DEFAULT_TENANT_ID'),
+                'language_id' => $randomLangage->language_id,
+                'default' => '1'
+            ]);
+            
+            $tenantLanguage = DB::table('tenant_language')
+            ->join('language', 'tenant_language.language_id', 'language.language_id')
+            ->where('tenant_id', env('DEFAULT_TENANT_ID'))
+            ->inRandomOrder()
+            ->first();
+        }
+        
+        $countryName = str_random(5);
+        $countryISO = str_random(2);
+
+        // Get random langauge for country name
+        $params = [
+            "countries" => [
+                [
+                    "iso" => $countryISO,
+                    "translations"=> [
+                        [
+                            "lang"=> $tenantLanguage->code,
+                            "name"=> $countryName
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $response = $this->post("countries", $params, ['Authorization' => 'Basic '.base64_encode(env('API_KEY').':'.env('API_SECRET'))])
+        ->seeStatusCode(201);
+
+        $countryId = json_decode($response->response->getContent())->data->country_ids[0]->country_id;
+        
+        DB::setDefaultConnection('mysql');
+
+        // Add another country with same ISO code        
+        $params = [
+            "countries" => [
+                [
+                    "iso" => $countryISO,
+                    "translations"=> [
+                        [
+                            "lang"=> $tenantLanguage->code,
+                            "name"=> $countryName
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        
+        $response = $this->post("countries", $params, ['Authorization' => 'Basic '.base64_encode(env('API_KEY').':'.env('API_SECRET'))])
+        ->seeStatusCode(422);
+
+        DB::setDefaultConnection('mysql');
+        
+        // Delete country and country_language data
+        $this->delete("countries/$countryId", [], ['Authorization' => 'Basic '.base64_encode(env('API_KEY').':'.env('API_SECRET'))])
+        ->seeStatusCode(204);
+
+        // Delete tenant language
+        DB::setDefaultConnection('mysql');
+        
+        DB::table('tenant_language')
+        ->where('tenant_language_id', $tenantLanguage->tenant_language_id)
+        ->delete();
+    } 
 }
