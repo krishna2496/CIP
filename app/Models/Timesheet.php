13d78a5bfcb8c5d4a936a1562186fb9800a8ac03
Dipen Suthar <dipen.suthar@tatvasoft.com>
2019-12-18 14:15:31 +0530

    Removed code for timesheet status

--- app/Models/Timesheet.php
+++ app/Models/Timesheet.php
@@ -3,7 +3,6 @@ namespace App\Models;
 
 use App\Models\Mission;
 use App\Models\TimesheetDocument;
-use App\Models\TimesheetStatus;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
