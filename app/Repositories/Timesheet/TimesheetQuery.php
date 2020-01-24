75cdd97ceff1c0b9d72346422b44de1c9ea91ee4
vdenaes <vdenaes@optimy.com>
2020-01-24 14:30:37 +0100

    fix the searchables columns

--- app/Repositories/Timesheet/TimesheetQuery.php
+++ app/Repositories/Timesheet/TimesheetQuery.php
@@ -10,13 +10,13 @@ use Illuminate\Support\Facades\Log;
 
 class TimesheetQuery implements QueryableInterface
 {
-    const FILTER_MISSION_THEMES     = 'missionThemes';
-    const FILTER_APPLICATION_DATE   = 'applicationDate';
-    const FILTER_MISSION_STATUSES   = 'customMissionStatus';
-    const FILTER_APPROVAL_STATUS    = 'timesheetStatus';
-    const FILTER_MISSION_COUNTRIES  = 'missionCountries';
-    const FILTER_MISSION_CITIES     = 'missionCities';
-    const FILTER_TIMESHEET_IDS     = 'timesheetIds';
+    const FILTER_MISSION_THEMES = 'missionThemes';
+    const FILTER_APPLICATION_DATE = 'applicationDate';
+    const FILTER_MISSION_STATUSES = 'customMissionStatus';
+    const FILTER_APPROVAL_STATUS = 'timesheetStatus';
+    const FILTER_MISSION_COUNTRIES = 'missionCountries';
+    const FILTER_MISSION_CITIES = 'missionCities';
+    const FILTER_TIMESHEET_IDS = 'timesheetIds';
 
     const ALLOWED_SORTABLE_FIELDS = [
         'applicant' => 'user.last_name',
@@ -55,12 +55,25 @@ class TimesheetQuery implements QueryableInterface
         $query = Timesheet::query();
         $timesheets = $query
             ->select([
-                'timesheet.*'
+                'timesheet.timesheet_id',
+                'timesheet.user_id',
+                'timesheet.mission_id',
+                'timesheet.time',
+                'timesheet.action',
+                'timesheet.date_volunteered',
+                'timesheet.day_volunteered',
+                'timesheet.notes',
+                'timesheet.status',
+                'timesheet.created_at',
+                'timesheet.updated_at',
+                'timesheet.deleted_at',
             ])
             ->join('user', 'user.user_id', '=', 'timesheet.user_id')
             ->whereHas('mission', function ($query) {
-                $query->where(['publication_status' => config("constants.publication_status")["APPROVED"],
-                    'mission_type' => config('constants.mission_type.TIME')]);
+                $query->where([
+                    'publication_status' => config("constants.publication_status")["APPROVED"],
+                    'mission_type' => config('constants.mission_type.TIME'),
+                ]);
             })
             ->whereHas('mission.missionApplication', function ($query) {
                 $query->whereIn('approval_status', [config("constants.application_status")["AUTOMATICALLY_APPROVED"]]);
@@ -80,78 +93,95 @@ class TimesheetQuery implements QueryableInterface
                 'mission.city.languages' => function ($query) use ($languageId) {
                     $query->where('language_id', '=', $languageId);
                 },
-                'timesheetDocument'
+                'timesheetDocument',
             ])
             // Filter by application start date
-            ->when(isset($filters[self::FILTER_APPLICATION_DATE]['from']), function($query) use ($filters) {
+            ->when(isset($filters[self::FILTER_APPLICATION_DATE]['from']), function ($query) use ($filters) {
                 $query->where('date_volunteered', '>=', $filters[self::FILTER_APPLICATION_DATE]['from']);
             })
             // Filter by application end date
-            ->when(isset($filters[self::FILTER_APPLICATION_DATE]['to']), function($query) use ($filters) {
+            ->when(isset($filters[self::FILTER_APPLICATION_DATE]['to']), function ($query) use ($filters) {
                 $query->where('date_volunteered', '<=', $filters[self::FILTER_APPLICATION_DATE]['to']);
             })
             // Filter by timesheet status
-            ->when(isset($filters[self::FILTER_APPROVAL_STATUS]), function($query) use ($filters) {
+            ->when(isset($filters[self::FILTER_APPROVAL_STATUS]), function ($query) use ($filters) {
                 $query->whereIn('timesheet.status',
                     collect($filters[self::FILTER_APPROVAL_STATUS])->map(function ($val) {
-                    return strtoupper($val);
+                        return strtoupper($val);
                     })
                 );
             })
-            ->when($hasMissionFilters, function($query) use ($filters) {
-                $query->whereHas('mission', function($query) use ($filters) {
+            ->when($hasMissionFilters, function ($query) use ($filters) {
+                $query->whereHas('mission', function ($query) use ($filters) {
                     // Filter by mission theme
-                    $query->when(isset($filters[self::FILTER_MISSION_THEMES]), function($query) use ($filters) {
+                    $query->when(isset($filters[self::FILTER_MISSION_THEMES]), function ($query) use ($filters) {
                         $query->whereIn('theme_id', $filters[self::FILTER_MISSION_THEMES]);
                     });
                     // Filter by mission country
-                    $query->when(isset($filters[self::FILTER_MISSION_COUNTRIES]), function($query) use ($filters) {
+                    $query->when(isset($filters[self::FILTER_MISSION_COUNTRIES]), function ($query) use ($filters) {
                         $query->whereIn('country_id', $filters[self::FILTER_MISSION_COUNTRIES]);
                     });
                     // Filter by mission city
-                    $query->when(isset($filters[self::FILTER_MISSION_CITIES]), function($query) use ($filters) {
+                    $query->when(isset($filters[self::FILTER_MISSION_CITIES]), function ($query) use ($filters) {
                         $query->whereIn('city_id', $filters[self::FILTER_MISSION_CITIES]);
                     });
                     // Filter by mission Status
-                    $query->when(isset($filters[self::FILTER_MISSION_STATUSES]), function($query) use ($filters) {
+                    $query->when(isset($filters[self::FILTER_MISSION_STATUSES]), function ($query) use ($filters) {
                         collect($filters[self::FILTER_MISSION_STATUSES])->map(function ($val) use ($query) {
                             if ($val === 'active') {
-                                return $query->whereIn('publication_status', ['PUBLISHED_FOR_APPLYING', 'APPROVED']); //TODO use constant ?
+                                return $query->whereIn('publication_status',
+                                    ['PUBLISHED_FOR_APPLYING', 'APPROVED']); //TODO use constant ?
                             } else {
                                 return $query->whereIn('publication_status', ['UNPUBLISHED', 'DRAFT']);
                             }
-                            });
+                        });
                     });
                 });
             })
             // Search
-            ->when(!empty($search), function($query) use ($search, $filters, $languageId) {
+            ->when(!empty($search), function ($query) use ($search, $filters, $languageId) {
                 $searchCallback = function ($query) use ($search, $languageId) {
-                    $query->whereHas('user', function($query) use ($search) {
-                        $query
-                            ->where('first_name', 'like', "%${search}%")
-                            ->orWhere('last_name', 'like', "%${search}%")
-                            ->orWhere('email', 'like', "%${search}%");
-                    })
-                        ->orwhereHas('mission.missionLanguage', function($query) use ($search, $languageId) {
+
+                    $query
+                        ->where('timesheet.status', 'like', "%${search}%")
+                        ->orWhere('timesheet.time', 'like', "%${search}%")
+                        ->orWhere('timesheet.notes', 'like', "%${search}%")
+                        ->orWhere('timesheet.day_volunteered', 'like', "%${search}%")
+//                        ->orWhere('timesheet.updated_at', 'like', "%${search}%")
+//                        ->orWhere('timesheet.created_at', 'like', "%${search}%")
+                        ->orwhereHas('timesheetDocument', function ($query) use ($search) {
+                            $query
+                                ->where('document_name', 'like', "%${search}%");
+                        })
+                        ->orwhereHas('mission.missionTheme', function ($query) use ($search) {
+                            $query
+                                ->where('theme_name', 'like', "%${search}%");
+                        })
+                        ->orwhereHas('user', function ($query) use ($search) {
+                            $query
+                                ->where('first_name', 'like', "%${search}%")
+                                ->orWhere('last_name', 'like', "%${search}%")
+                                ->orWhere('email', 'like', "%${search}%");
+                        })
+                        ->orwhereHas('mission.missionLanguage', function ($query) use ($search, $languageId) {
                             $query
                                 ->where([
                                     ['title', 'like', "%${search}%"],
-                                    ['language_id', '=', $languageId]
+                                    ['language_id', '=', $languageId],
                                 ]);
                         })
-                        ->orwhereHas('mission.city.languages', function($query) use ($search, $languageId) {
+                        ->orwhereHas('mission.city.languages', function ($query) use ($search, $languageId) {
                             $query
                                 ->where([
                                     ['name', 'like', "%${search}%"],
-                                    ['language_id', '=', $languageId]
+                                    ['language_id', '=', $languageId],
                                 ]);
                         })
-                        ->orwhereHas('mission.country.languages', function($query) use ($search, $languageId) {
+                        ->orwhereHas('mission.country.languages', function ($query) use ($search, $languageId) {
                             $query
                                 ->where([
                                     ['name', 'like', "%${search}%"],
-                                    ['language_id', '=', $languageId]
+                                    ['language_id', '=', $languageId],
                                 ]);
                         });
                 };
@@ -182,13 +212,17 @@ class TimesheetQuery implements QueryableInterface
     private function getFilteringLanguage(array $filters, Collection $tenantLanguages): int
     {
         $hasLanguageFilter = array_key_exists('language', $filters);
-        $defaultLanguageId = $tenantLanguages->filter(function ($language) use ($filters) { return $language->default == 1; })->first()->language_id;
+        $defaultLanguageId = $tenantLanguages->filter(function ($language) use ($filters) {
+            return $language->default == 1;
+        })->first()->language_id;
 
         if (!$hasLanguageFilter) {
             return $defaultLanguageId;
         }
 
-        $language = $tenantLanguages->filter(function ($language) use ($filters) { return $language->code === $filters['language']; })->first();
+        $language = $tenantLanguages->filter(function ($language) use ($filters) {
+            return $language->code === $filters['language'];
+        })->first();
 
         if (is_null($language)) {
             return $defaultLanguageId;
