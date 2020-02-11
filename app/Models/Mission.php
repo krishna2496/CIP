a0092618e6a265faf0cb68e677cd3f51077978bd
Jonathan Benitez <jbenitez@optimy.com>
2020-02-11 19:01:21 +0800

    update on ci platform user timesheets

--- app/Models/Mission.php
+++ app/Models/Mission.php
@@ -77,7 +77,7 @@ class Mission extends Model
     'user_application_status', 'skill', 'rating', 'mission_rating_total_volunteers',
     'availability_id', 'availability_type', 'average_rating', 'timesheet', 'total_hours', 'time',
     'hours', 'action', 'ISO', 'total_minutes', 'custom_information', 'total_timesheet_time', 'total_timesheet_action', 'total_timesheet',
-    'mission_title'];
+    'mission_title', 'mission_objective'];
     
     /**
      * Get the document record associated with the mission.
