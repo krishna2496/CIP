1030c9677a2823fa6827e5685e7cb70d9338b80b
Veronique Fauvarque <vfauvarque@optimy.com>
2020-02-23 22:06:42 +0100

    conditional join when mission is goal

--- app/Repositories/Timesheet/TimesheetQuery.php
+++ app/Repositories/Timesheet/TimesheetQuery.php
@@ -6,6 +6,7 @@ use App\Models\Timesheet;
 use App\Repositories\Core\QueryableInterface;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 
 class TimesheetQuery implements QueryableInterface
 {
@@ -68,11 +69,13 @@ class TimesheetQuery implements QueryableInterface
                 'mission_language.title',
                 'city_language.name',
                 'country_language.name',
-                'goal_mission.goal_objective'
+                $filters[self::FILTER_TYPE] !== 'goal' ? 'timesheet.time' :'goal_mission.goal_objective'
             ])
             ->join('user', 'user.user_id', '=', 'timesheet.user_id')
             ->join('mission', 'mission.mission_id', '=', 'timesheet.mission_id')
-            ->join('goal_mission', 'goal_mission.mission_id', '=', 'timesheet.mission_id')
+            ->when($filters[self::FILTER_TYPE] === 'goal', function ($query) use ($filters) {
+                $query->join('goal_mission', 'goal_mission.mission_id', '=', 'timesheet.mission_id');
+            })
             ->join('mission_language', function ($join) use ($languageId) {
                 $join->on('mission_language.mission_id', '=', 'timesheet.mission_id')
                     ->where('mission_language.language_id', '=', $languageId);
