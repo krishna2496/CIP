5dce6bdf485812bfe6ca6ab7e100610d60ffc8d7
Siddharajsinh Zala <siddharajsinh.zala@tatvasoft.com>
2020-02-26 11:55:16 +0530

    Added try catch block in localization middleware

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -2,7 +2,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Response;
 use App\Helpers\LanguageHelper;
+use App\Helpers\ResponseHelper;
 
 class LocalizationMiddleware
 {
@@ -12,13 +14,19 @@ class LocalizationMiddleware
     private $languageHelper;
 
     /**
+     * @var App\Helpers\ResponseHelper
+     */
+    private $responseHelper;
+    
+    /**
      * Create a new localization middleware instance.
      *
      * @param App\Helpers\LanguageHelper $languageHelper
      * @return void
      */
-    public function __construct(LanguageHelper $languageHelper)
+    public function __construct(LanguageHelper $languageHelper, ResponseHelper $responseHelper)
     {
+        $this->responseHelper = $responseHelper;
         $this->languageHelper = $languageHelper;
     }
     
@@ -33,9 +41,18 @@ class LocalizationMiddleware
     {
         // Set localization to config locale
         config(['app.locale' => $request->header('X-localization')]);
-
-        // Get tenant language base on localization or default language of tenant from database
-        $language = $this->languageHelper->checkTenantLanguage($request);
+        try {
+            // Get tenant language base on localization or default language of tenant from database
+            $language = $this->languageHelper->checkTenantLanguage($request);
+        } catch (\Exception $e) {
+            // Send authentication error response if api user not found in master database
+            return $this->responseHelper->error(
+                Response::HTTP_UNAUTHORIZED,
+                Response::$statusTexts[Response::HTTP_UNAUTHORIZED],
+                config('constants.error_codes.ERROR_INVALID_API_AND_SECRET_KEY'),
+                trans('messages.custom_error_message.ERROR_INVALID_API_AND_SECRET_KEY')
+            );
+        }
         
         // set laravel localization
         app('translator')->setLocale($language->code);
