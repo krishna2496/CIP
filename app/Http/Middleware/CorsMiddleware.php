f999d7eb6ad89bb89b0ec4d1c1fecd1b67317e74
Veronique Fauvarque <vfauvarque@optimy.com>
2020-02-28 14:48:17 +0100

    fix export in the frontend

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,6 +1,9 @@
 <?php namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class CorsMiddleware
 {
@@ -14,7 +17,6 @@ class CorsMiddleware
 	public function handle($request, Closure $next)
 	{
 		$headers = [
-			'Access-Control-Allow-Origin'      => '*',
 			'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE, PATCH',
 			'Access-Control-Allow-Credentials' => 'true',
 			'Access-Control-Max-Age'           => '86400',
@@ -29,8 +31,15 @@ class CorsMiddleware
 		}
 
 		$response = $next($request);
-		foreach($headers as $key => $value)
-		{
+
+		if (get_class($response) === 'Symfony\Component\HttpFoundation\BinaryFileResponse') {
+		    foreach($headers as $key => $value) {
+			    $response->headers->set($key, $value);
+		    }
+		    return $response;
+        }
+
+		foreach($headers as $key => $value) {
 			$response->header($key, $value);
 		}
 
