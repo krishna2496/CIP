52753c70caee77b1e74aebf1e68215ae44841b7f
Quentin Delcourt <qdelcourt@optimy.com>
2020-03-12 10:12:37 +0100

    Add API translations

--- resources/lang/fr/mail-subjects.php
+++ resources/lang/fr/mail-subjects.php
@@ -2,15 +2,16 @@
 
 return [
     "subjects" => [
-        "NEW_MISSIONS" => "Création d'une nouvelle mission",
+        "NEW_MISSIONS" => "Nouvelle mission créée",
         "NEW_MESSAGES" => "Nouveau message reçu",
         "MY_COMMENTS" => "Votre commentaire sur la mission",
-        "NEW_NEWS" => "Nouvelles nouvelles publiées",
-        "MISSION_APPLICATION" => "Mise à jour de votre application de mission",
-        "MY_STORIES" => "Le statut de votre histoire",
-        "VOLUNTEERING_GOALS" => "L'état de votre feuille de temps",
-        "VOLUNTEERING_HOURS" => "L'état de votre feuille de temps",
-        "RECOMMENDED_STORY" => "Votre recommandation d'histoire",
-        "RECOMMENEDE_MISSION" => "Votre recommandation de mission",
+        "NEW_NEWS" => "Nouvelles actualités publiées",
+        "MISSION_APPLICATION" => "Mise à jour de votre mission d’application",
+        "MY_STORIES" => "Votre statut de récit",
+        "VOLUNTEERING_GOALS" => "Statut de votre feuille de temps",
+        "VOLUNTEERING_HOURS" => "Statut de votre feuille de temps",
+        "RECOMMENDED_STORY" => "Recommandation de votre récit",
+        "RECOMMENEDE_MISSION" => "Recommandation de votre mission",
     ]
 ];
+?>
