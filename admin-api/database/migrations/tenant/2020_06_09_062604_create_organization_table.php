d054f922094e2f4e144e20425fb263a207b637b0
Dhaval Prajapati <dhaval.prajapati@tatvasoft.com>
2020-06-09 14:40:34 +0530

    organization model and migrations

---
+++ admin-api/database/migrations/tenant/2020_06_09_062604_create_organization_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name',255);
+            $table->integer('legal_number');
+            $table->integer('phone_number');
+            $table->string('address_line_1',255);
+            $table->string('address_line_2',255);
+            $table->unsignedBigInteger('city_id');
+            $table->unsignedBigInteger('state_id');
+            $table->unsignedBigInteger('country_id');
+            $table->integer('postal_code');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('city_id')->references('city_id')->on('city')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('state_id')->references('state_id')->on('city')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('country_id')->references('country_id')->on('country')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization');
+    }
+}
---
+++ api/app/Models/Organization.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Iatstuti\Database\Support\CascadeSoftDeletes;
+
+class Organization extends Model
+{
+    use SoftDeletes, CascadeSoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'organization';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'legal_number', 'phone_number', 'address_line_1', 'address_line_2','city_id','state_id','country_id','postal_code'];
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that should be visible in arrays.
+     *
+     * @var array
+     */
+    protected $visible = [
+        'name',
+        'legal_number',
+        'phone_number',
+        'address_line_1',
+        'address_line_2',
+        'city_id',
+        'state_id',
+        'country_id',
+        'postal_code',
+    ];
+
+}
