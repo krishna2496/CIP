e6dedae300756b7d4a37490f4e1a9dc1973bf030
krishna Dhanani <krishna.dhanani@tatvasoft.com>
2020-06-10 18:27:46 +0530

    migration script to remove volunteering attribute

---
+++ admin-api/bin/php/oneShot/CIP-451-migration-to-remove-volunteering-attributes-from-mission/migration_to_remove_volunteering_attributes_from_mission.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('../../../../bootstrap/app.php');
+use Illuminate\Support\Str;
+
+$db = app()->make('db');
+
+$pdo = $db->connection('mysql')->getPdo();
+
+\Illuminate\Support\Facades\Config::set('database.default', 'mysql');
+$tenants = $pdo->query('select * from tenant where status=1')->fetchAll();
+
+if (count($tenants) > 0) {
+    foreach ($tenants as $tenant) {
+        $tenantId = $tenant['tenant_id'];
+        $db->purge('tenant');
+        // Create connection to tenant
+        \Illuminate\Support\Facades\Config::set('database.connections.tenant', array(
+            'driver' => 'mysql',
+            'host' => env('DB_HOST'),
+            'database' => 'ci_tenant_'.$tenantId,
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+        ));
+        // Create connection for the tenant database
+        $pdo = $db->connection('tenant')->getPdo();
+        
+        // Set default database
+        \Illuminate\Support\Facades\Config::set('database.default', 'tenant');
+        
+        $pdo->exec('ALTER TABLE mission 
+            DROP FOREIGN KEY mission_availability_id_foreign
+        ');
+
+        $missionData = $pdo->exec('ALTER TABLE mission 
+            DROP COLUMN availability_id,
+            DROP COLUMN total_seats,
+            DROP COLUMN is_virtual
+        ');
+    }
+}
