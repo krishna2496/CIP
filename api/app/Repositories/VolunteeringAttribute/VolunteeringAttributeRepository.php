24c3215e1880dcac7fd02e1cb3801fec8041f968
krishna Dhanani <krishna.dhanani@tatvasoft.com>
2020-06-12 10:28:36 +0530

    volunteering attribute repository changes

--- api/app/Repositories/VolunteeringAttribute/VolunteeringAttributeRepository.php
+++ api/app/Repositories/VolunteeringAttribute/VolunteeringAttributeRepository.php
@@ -9,14 +9,18 @@ use App\Traits\RestExceptionHandlerTrait;
 class VolunteeringAttributeRepository implements VolunteeringAttributeInterface
 {
     use RestExceptionHandlerTrait;
+    /**
+     * @var App\Models\VolunteeringAttribute
+     */
+    public $volunteeringAttribute;
     
     /**
-     * Create a new user filter repository instance.
+     * Create a new volunteering attribute repository instance.
      *
-     * @param  App\Models\VolunteeringAttribute $filters
+     * @param  App\Models\VolunteeringAttribute $volunteeringAttribute
      * @return void
      */
-    public function __construct(VolunteeringAttribute $filters)
+    public function __construct(VolunteeringAttribute $volunteeringAttribute)
     {
         
     }
