a439bd3c89faf84f07f5f6d1d2da931ca64d9c48
krishna Dhanani <krishna.dhanani@tatvasoft.com>
2020-06-25 09:23:13 +0530

    syntax error and or condition added

--- api/app/Http/Controllers/Admin/State/StateController.php
+++ api/app/Http/Controllers/Admin/State/StateController.php
@@ -136,7 +136,7 @@ class StateController extends Controller
             foreach ($request->states[0]['translations'] as $key => $value) {
                 $languageCode = $value['lang'];
                 // Check for valid language code inside tenant and ci admin
-                if (!$this->languageHelper->isValidAdminLanguageCode($languageCode) &&
+                if (!$this->languageHelper->isValidAdminLanguageCode($languageCode) ||
                 !$this->languageHelper->isValidTenantLanguageCode($request, $languageCode)) {
                     return $this->responseHelper->error(
                         Response::HTTP_UNPROCESSABLE_ENTITY,
@@ -205,7 +205,7 @@ class StateController extends Controller
                 foreach ($request->translations as $key => $value) {
                     $languageCode = $value['lang'];
                     // Check for valid language code
-                    if (!$this->languageHelper->isValidAdminLanguageCode($languageCode) &&
+                    if (!$this->languageHelper->isValidAdminLanguageCode($languageCode) ||
                     !$this->languageHelper->isValidTenantLanguageCode($request, $languageCode)) {
                         return $this->responseHelper->error(
                             Response::HTTP_UNPROCESSABLE_ENTITY,
