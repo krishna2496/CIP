aae8d52d2b05bf8a93f76b4f49bb6e186c9c3637
Dhaval Prajapati <dhaval.prajapati@tatvasoft.com>
2020-07-07 19:32:10 +0530

    remove time and goal logic

--- api/app/Http/Middleware/TenantHasSettings.php
+++ api/app/Http/Middleware/TenantHasSettings.php
@@ -14,13 +14,13 @@ class TenantHasSettings
      */
     private $tenantActivatedSettingRepository;
 
-     /**
-     * Create a new Tenant has setting instance
-     *
-     * @param App\Repositories\TenantActivatedSetting\TenantActivatedSettingRepository
-     * @param App\Helpers\ResponseHelper $responseHelper
-     * @return void
-     */
+    /**
+    * Create a new Tenant has setting instance
+    *
+    * @param App\Repositories\TenantActivatedSetting\TenantActivatedSettingRepository
+    * @param App\Helpers\ResponseHelper $responseHelper
+    * @return void
+    */
     public function __construct(
         TenantActivatedSettingRepository $tenantActivatedSettingRepository,
         ResponseHelper $responseHelper
@@ -34,24 +34,11 @@ class TenantHasSettings
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  array $settings 
+     * @param  array $settings
      * @return mixed
      */
     public function handle($request, Closure $next, ...$settings)
     {
-        // Check for mission post/patch api
-        $routeName = (!empty($request->route()[1])) ? $request->route()[1]['as'] : false;
-        if ($routeName && ($routeName == 'missions.store' || $routeName == 'missions.update')) {
-            $result = $this->missionSettingsPermissions($request, $request->get('mission_type'), $settings);
-            if (!$result) {
-                return $this->responseHelper->error(
-                    Response::HTTP_FORBIDDEN,
-                    Response::$statusTexts[Response::HTTP_FORBIDDEN],
-                    '',
-                    trans('messages.custom_error_message.ERROR_UNAUTHORIZED')
-                );
-            }
-        }
         foreach ($settings as $key => $setting) {
             $result = $this->tenantActivatedSettingRepository->checkTenantSettingStatus(
                 $setting,
@@ -68,35 +55,5 @@ class TenantHasSettings
         }
         $response = $next($request);
         return $response;
-    }
-
-    public function missionSettingsPermissions($request, $type, $settings)
-    {
-        $volunteering = false;
-        // check for volunteering settings
-        if (in_array("volunteering", $settings)) {
-            $result = $this->tenantActivatedSettingRepository->checkTenantSettingStatus(
-                'volunteering',
-                $request
-            );
-            $volunteering = true;
-        }
-        // check for goal settings
-        if (($type == 'GOAL') & in_array("goal", $settings)) {
-            $result = $this->tenantActivatedSettingRepository->checkTenantSettingStatus(
-                'goal',
-                $request
-            );
-            return ($result & $volunteering) ? true : false;
-        }
-        // check for time settings
-        if (($type == 'TIME') & in_array("time", $settings)) {
-            $result = $this->tenantActivatedSettingRepository->checkTenantSettingStatus(
-                'time',
-                $request
-            );
-            return ($result & $volunteering) ? true : false;
-        }
-        return false;
     }
 }
