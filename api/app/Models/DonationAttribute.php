7db90cc9a7fa54ee9e6f286dfd2d544924e6079d
krishna Dhanani <krishna.dhanani@tatvasoft.com>
2020-07-14 11:39:35 +0530

    donation attribute related changes

--- api/app/Models/DonationAttribute.php
+++ api/app/Models/DonationAttribute.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Support\Str;
 class DonationAttribute extends Model
 {
     use SoftDeletes;
@@ -38,4 +38,20 @@ class DonationAttribute extends Model
      */
     protected $visible = ['goal_amount_currency', 'goal_amount', 'show_goal_amount', 'show_donation_percentage', 'show_donation_meter', 'show_donation_count',
     'show_donors_count', 'disable_when_funded', 'is_disabled', ];
+	
+	/**
+     * listen for any Eloquent events
+     *
+     * @return void
+     */
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function ($donationAttribute) {
+            if (! $donationAttribute->getKey()) {
+                $donationAttribute->{$donationAttribute->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
 }
