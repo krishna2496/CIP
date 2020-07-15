03dc72fd8fd14ef074d974e58216de830c68bab1
Ravi chauahan <ravi.chauhan@tatvasoft.com>
2020-07-15 14:30:51 +0530

    Validation changes for impact donation mission

--- api/app/Http/Controllers/Admin/Mission/MissionController.php
+++ api/app/Http/Controllers/Admin/Mission/MissionController.php
@@ -168,7 +168,7 @@ class MissionController extends Controller
                 "is_virtual" => "sometimes|required|in:0,1",
                 "mission_detail.*.label_goal_achieved" => 'sometimes|required_if:mission_type,GOAL|max:255',
                 "mission_detail.*.label_goal_objective" => 'sometimes|required_if:mission_type,GOAL|max:255',
-                "impact_donation.*.amount" => 'required|integer',
+                "impact_donation.*.amount" => 'required|integer|min:1',
                 "impact_donation.*.translations" => 'required',
                 "impact_donation.*.translations.*.language_code" => 
                 'required_with:impact_donation.*.translations|max:2',
@@ -298,13 +298,13 @@ class MissionController extends Controller
                 "impact_donation.*.impact_donation_id" =>
                 'sometimes|required|exists:mission_impact_donation,mission_impact_donation_id,deleted_at,NULL',
                 "impact_donation.*.amount" =>
-                "required_without:impact_donation.*.impact_donation_id|integer",
+                "required_without:impact_donation.*.impact_donation_id|integer|min:1",
                 "impact_donation.*.translations" =>
                 "required_without:impact_donation.*.impact_donation_id",
                 "impact_donation.*.translations.*.language_code" =>
                 "required_with:impact_donation.*.translations|max:2",
                 "impact_donation.*.translations.*.content" =>
-                "required_with:impact_donation.*.translations",
+                "required_with:impact_donation.*.translations|max:160",
             ]
         );
 
