78e66a29def130aadbea646799c3ab3a183118e1
krishna Dhanani <krishna.dhanani@tatvasoft.com>
2020-07-15 19:20:04 +0530

    mission get api changes

--- api/app/Models/DonationAttribute.php
+++ api/app/Models/DonationAttribute.php
@@ -31,11 +31,29 @@ class DonationAttribute extends Model
     protected $fillable = ['mission_id', 'goal_amount_currency', 'goal_amount', 'show_goal_amount', 'show_donation_percentage', 'show_donation_meter', 'show_donation_count',
     'show_donors_count', 'disable_when_funded', 'is_disabled', ];
 
+    // Donation related static code added,because donation related functionality is pending for development
+    
+    protected $appends = ['donation_amount_raised', 'donor_count', 'donation_count'];
     /**
      * The attributes that should be visible in arrays.
      *
      * @var array
      */
     protected $visible = ['goal_amount_currency', 'goal_amount', 'show_goal_amount', 'show_donation_percentage', 'show_donation_meter', 'show_donation_count',
-    'show_donors_count', 'disable_when_funded', 'is_disabled', ];
+    'show_donors_count', 'disable_when_funded', 'is_disabled', 'donation_amount_raised', 'donor_count', 'donation_count'];
+
+    public function getDonationAmountRaisedAttribute()
+    {
+        return 358;
+    }
+
+    public function getDonorCountAttribute()
+    {
+        return 21;
+    }
+
+    public function getDonationCountAttribute()
+    {
+        return 78;
+    }
 }
