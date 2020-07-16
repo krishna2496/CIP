86058d657945914bb1fe85174e3a873c3d1e2c5e
krishna Dhanani <krishna.dhanani@tatvasoft.com>
2020-07-16 17:41:32 +0530

    donation attribute button changes

--- frontend/src/views/DonationMissionDetail.vue
+++ frontend/src/views/DonationMissionDetail.vue
@@ -146,7 +146,7 @@
                                     </i>
                                     <span>{{ languageData.label.recommend_to_co_worker }}</span>
                                 </b-button>
-                                <b-button class="btn-borderprimary icon-btn remind-icon">
+                                <b-button class="btn-borderprimary icon-btn remind-icon gray-btn">
                                     <i>
                                         <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 11 15" style="enable-background:new 0 0 11 15;" xml:space="preserve">
                                             <g>
