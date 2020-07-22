e8136d53d6d60df59f2f2782058892ea46ac031a
Dhaval Prajapati <dhaval.prajapati@tatvasoft.com>
2020-07-22 11:01:57 +0530

    model and migration file update

--- api/app/Models/Organization.php
+++ api/app/Models/Organization.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Iatstuti\Database\Support\CascadeSoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class Organization extends Model
 {
@@ -22,14 +23,23 @@ class Organization extends Model
      *
      * @var string
      */
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'organization_id';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['name', 'legal_number', 'phone_number', 'address_line_1', 'address_line_2','city_id','state_id','country_id','postal_code'];
+    protected $fillable = [
+        'name',
+        'legal_number',
+        'phone_number',
+        'address_line_1',
+        'address_line_2',
+        'city_id','state_id',
+        'country_id',
+        'postal_code'
+    ];
 
     /**
      * Indicates if the IDs are auto-incrementing.
@@ -55,4 +65,18 @@ class Organization extends Model
         'postal_code',
     ];
 
+    /**
+     * Binds creating/saving events to create UUIDs.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            // Generate UUID
+            $model->organization_id = Uuid::uuid4()->toString();
+        });
+    }
 }
