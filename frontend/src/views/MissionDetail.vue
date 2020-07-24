7dd8cc851af38b66da9587c1d01116f46b0d8649
krishna Dhanani <krishna.dhanani@tatvasoft.com>
2020-07-24 19:44:11 +0530

    title changes

--- frontend/src/views/MissionDetail.vue
+++ frontend/src/views/MissionDetail.vue
@@ -269,12 +269,12 @@ Add entry
                                                 </network>
                                             </div>
                                             <div class="social-icon">
-                                                <img :src="$store.state.imagePath+'/assets/images/linkedin-ic-grey.svg'" :alt="`${JSON.parse(this.$store.state.languageLabel).label.twitter}`" :title="`${JSON.parse(this.$store.state.languageLabel).label.linkedin}`" class="normal-img" />
-                                                <img :src="$store.state.imagePath+'/assets/images/linkedin-ic-white.svg'" :alt="`${JSON.parse(this.$store.state.languageLabel).label.twitter}`" :title="`${JSON.parse(this.$store.state.languageLabel).label.linkedin}`" class="hover-img" />
+                                                <img :src="$store.state.imagePath+'/assets/images/linkedin-ic-grey.svg'" :alt="`${JSON.parse(this.$store.state.languageLabel).label.linkedin}`" :title="`${JSON.parse(this.$store.state.languageLabel).label.linkedin}`" class="normal-img" />
+                                                <img :src="$store.state.imagePath+'/assets/images/linkedin-ic-white.svg'" :alt="`${JSON.parse(this.$store.state.languageLabel).label.linkedin}`" :title="`${JSON.parse(this.$store.state.languageLabel).label.linkedin}`" class="hover-img" />
                                             </div>
                                             <div class="social-icon">
-                                                <img :src="$store.state.imagePath+'/assets/images/link-ic-grey.svg'" :alt="`${JSON.parse(this.$store.state.languageLabel).label.twitter}`" :title="`${JSON.parse(this.$store.state.languageLabel).label.twitter}`" class="normal-img" />
-                                                <img :src="$store.state.imagePath+'/assets/images/link-ic-white.svg'" :alt="`${JSON.parse(this.$store.state.languageLabel).label.twitter}`" :title="`${JSON.parse(this.$store.state.languageLabel).label.twitter}`" class="hover-img" />
+                                                <img :src="$store.state.imagePath+'/assets/images/link-ic-grey.svg'" :alt="`${JSON.parse(this.$store.state.languageLabel).label.link}`" :title="`${JSON.parse(this.$store.state.languageLabel).label.twitter}`" class="normal-img" />
+                                                <img :src="$store.state.imagePath+'/assets/images/link-ic-white.svg'" :alt="`${JSON.parse(this.$store.state.languageLabel).label.link}`" :title="`${JSON.parse(this.$store.state.languageLabel).label.twitter}`" class="hover-img" />
                                             </div>
                                         </div>
                                     </social-sharing>
