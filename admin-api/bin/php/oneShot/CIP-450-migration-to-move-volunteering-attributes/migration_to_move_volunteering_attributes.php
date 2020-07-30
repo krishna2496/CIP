231e7b0e00c685d02616996f5dcc39f2d2aa2798
krishna <krishna.dhanani@tatvasoft.com>
2020-07-30 18:54:24 +0530

    volunteering attribute changes

--- admin-api/bin/php/oneShot/CIP-450-migration-to-move-volunteering-attributes/migration_to_move_volunteering_attributes.php
+++ admin-api/bin/php/oneShot/CIP-450-migration-to-move-volunteering-attributes/migration_to_move_volunteering_attributes.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once('../../../../bootstrap/app.php');
-use Illuminate\Support\Str;
+require_once(__DIR__.'/../../../../bootstrap/app.php');
+use Ramsey\Uuid\Uuid;
 
 $db = app()->make('db');
 
 $pdo = $db->connection('mysql')->getPdo();
 
 \Illuminate\Support\Facades\Config::set('database.default', 'mysql');
-$tenants = $pdo->query('select * from tenant where status=1 and deleted_at IS NULL')->fetchAll();
+$tenants = $pdo->query('select * from tenant where status = 1 and deleted_at IS NULL')->fetchAll();
 
 if (count($tenants) > 0) {
     foreach ($tenants as $tenant) {
@@ -28,17 +28,16 @@ if (count($tenants) > 0) {
         // Set default database
         \Illuminate\Support\Facades\Config::set('database.default', 'tenant');
 
-        $missionData = $pdo->query('select mission_id,total_seats,availability_id,is_virtual from mission')->fetchAll();
-        
+        $missionData = $pdo->query('select mission_id, total_seats, availability_id, is_virtual from mission')->fetchAll();
         if (!empty($missionData)) {
             foreach ($missionData as $mission) {
                     $pdo->prepare('
                         INSERT INTO volunteering_attribute
-                        (volunteering_attribute_id,mission_id,availability_id,total_seats,is_virtual,created_at)
-                        VALUES (:volunteering_attribute_id,:mission_id,:availability_id,:total_seats,:is_virtual,:created_at)
+                        (volunteering_attribute_id, mission_id, availability_id, total_seats, is_virtual, created_at)
+                        VALUES (:volunteering_attribute_id, :mission_id, :availability_id, :total_seats, :is_virtual, :created_at)
                     ')
                         ->execute([
-                            'volunteering_attribute_id' => (String) Str::uuid(),
+                            'volunteering_attribute_id' => Uuid::uuid4()->toString(),
                             'mission_id' => $mission['mission_id'],
                             'availability_id' => $mission['availability_id'],
                             'total_seats' => $mission['total_seats'],
