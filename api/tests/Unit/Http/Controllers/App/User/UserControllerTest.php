aa6b70cbc784a6b001a9cec96513fa0947f75424
Christopher Buenafe <christopherbuenafe@gmail.com>
2020-08-03 16:21:22 +0800

    CIP-624 updated unit tests

--- api/tests/Unit/Http/Controllers/App/User/UserControllerTest.php
+++ api/tests/Unit/Http/Controllers/App/User/UserControllerTest.php
@@ -14,6 +14,7 @@ use App\Repositories\UserCustomField\UserCustomFieldRepository;
 use App\Repositories\UserFilter\UserFilterRepository;
 use App\Repositories\User\UserRepository;
 use App\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Mockery;
@@ -21,20 +22,21 @@ use TestCase;
 
 class UserControllerTest extends TestCase
 {
+    const OPTION_NAME_SSO = 'saml_settings';
+
     public function testInviteUser()
     {
         $request = $this->mock(Request::class);
         $request->shouldReceive('toArray')
             ->andReturn([
-                'email' => 'test@optimy.com'
+                'email' => 'test@optimy.com',
+                'subject' => 'Notification',
+                'body' => 'body',
+                'language' => 'en'
             ])
             ->shouldReceive('get')
             ->andReturn('test@optimy.com')
-            ->shouldReceive('all')
-            ->shouldReceive('route')
-            ->shouldReceive('secure');
-
-        $userModel = new User();
+            ->shouldReceive('only');
 
         $userModel = $this->mock(User::class);
         $userModel->shouldReceive('getAttribute')
@@ -49,26 +51,36 @@ class UserControllerTest extends TestCase
         $userCustomFieldRepository = $this->mock(UserCustomFieldRepository::class);
         $cityRepository = $this->mock(CityRepository::class);
         $userFilterRepository = $this->mock(UserFilterRepository::class);
+
         $responseHelper = $this->mock(ResponseHelper::class);
         $responseHelper->shouldReceive('success');
 
         $languageHelper = $this->mock(LanguageHelper::class);
-        $languageHelper->shouldReceive('getLanguageDetails')
-            ->andReturn($request);
 
         $helpers = $this->mock(Helpers::class);
-        $helpers->shouldReceive('getSubDomainFromRequest');
 
         $s3Helper = $this->mock(S3Helper::class);
+
+        $samlSettings = new Collection([
+            0 => [
+                'option_value' => [
+                    'saml_access_only' => false
+                ]
+            ]
+        ]);
 
         $tenantOption = $this->mock(TenantOption::class);
         $tenantOption->shouldReceive('getAttribute');
 
         $tenantOptionRepository = $this->mock(TenantOptionRepository::class);
-        $tenantOptionRepository->shouldReceive('getOptionValueFromOptionName')
+        $tenantOptionRepository->shouldReceive('getOptionValue')
+            ->with(self::OPTION_NAME_SSO)
+            ->once()
+            ->andReturn($samlSettings)
+            ->shouldReceive('getOptionValueFromOptionName')
             ->andReturn($tenantOption);
 
-        $authController = new UserController(
+        $userController = new UserController(
             $userRepository,
             $userCustomFieldRepository,
             $cityRepository,
@@ -82,7 +94,62 @@ class UserControllerTest extends TestCase
 
         $this->withoutEvents();
 
-        $response = $authController->inviteUser($userModel, $request);
+        $response = $userController->inviteUser($userModel, $request);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+    }
+
+    public function testCreatePassword()
+    {
+        $request = $this->mock(Request::class);
+        $request->shouldReceive('toArray')
+            ->andReturn([
+                'email' => 'test@optimy.com',
+                'password' => 'YwE$#12dW'
+            ])
+            ->shouldReceive('get')
+            ->andReturn('test@optimy.com')
+            ->shouldReceive('only');
+
+        $userModel = $this->mock(User::class);
+        $userModel->shouldReceive('getAttribute')
+            ->shouldReceive('setAttribute')
+            ->shouldReceive('save');
+
+        $userRepository = $this->mock(UserRepository::class);
+        $userRepository->shouldReceive('findUserByEmail')
+            ->andReturn($userModel);
+
+        $userCustomFieldRepository = $this->mock(UserCustomFieldRepository::class);
+        $cityRepository = $this->mock(CityRepository::class);
+        $userFilterRepository = $this->mock(UserFilterRepository::class);
+
+        $responseHelper = $this->mock(ResponseHelper::class);
+        $responseHelper->shouldReceive('success');
+
+        $languageHelper = $this->mock(LanguageHelper::class);
+
+        $helpers = $this->mock(Helpers::class);
+
+        $s3Helper = $this->mock(S3Helper::class);
+
+        $tenantOptionRepository = $this->mock(TenantOptionRepository::class);
+
+        $userController = new UserController(
+            $userRepository,
+            $userCustomFieldRepository,
+            $cityRepository,
+            $userFilterRepository,
+            $responseHelper,
+            $languageHelper,
+            $helpers,
+            $s3Helper,
+            $tenantOptionRepository
+        );
+
+        $this->withoutEvents();
+
+        $response = $userController->createPassword($request);
 
         $this->assertInstanceOf(JsonResponse::class, $response);
     }
