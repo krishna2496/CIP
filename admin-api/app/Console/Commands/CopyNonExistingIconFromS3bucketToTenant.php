73fb53434d43eb047f80b05e6103b7802fa965a0
Ravi Chauhan <ravi.chauhan@tatvasoft.com>
2020-08-06 15:11:35 +0530

    artisan custom command task CIP-649

--- admin-api/app/Console/Commands/CopyNonExistingIconFromS3bucketToTenant.php
+++ admin-api/app/Console/Commands/CopyNonExistingIconFromS3bucketToTenant.php
@@ -14,7 +14,7 @@ class CopyNonExistingIconFromS3bucketToTenant extends Command
      *
      * @var string
      */
-    protected $signature = 'copy:new-icons {--path=} {--file=}';
+    protected $signature = 'copy:new-icons {--folder=} {--file=}';
 
     /**
      * The console command description.
@@ -54,59 +54,79 @@ class CopyNonExistingIconFromS3bucketToTenant extends Command
      */
     public function handle()
     {
-        $folderPath = $this->option('path');
+        // --path option is available
+        $folderPath = $this->option('folder');
+
+        // --file option is available
         $filePath = $this->option('file');
 
         if (!empty($folderPath) && empty($filePath)) {
-            $tenants = $this->tenantRepository->getAllTenants();
-            $bar = $this->output->createProgressBar($tenants->count());
-            if ($tenants->count() > 0) {
 
-                // Copy default_theme folder which is already present on S3
-                $files = Storage::disk('s3')->allFiles(env('AWS_S3_DEFAULT_THEME_FOLDER_NAME').
-                '/'.env('AWS_S3_ASSETS_FOLDER_NAME'));
+            //check --path option directory is exist or not
+            if (Storage::disk('s3')->exists($folderPath)) {
+                $files = Storage::disk('s3')->allFiles($folderPath);
+                $tenants = $this->tenantRepository->getAllTenants();
+                $option = 'folder';
 
-                $tenantsList = $tenants->toArray();
-                $this->info('Total tenants : '. $tenants->count());
-                $this->info("\nIt is going to save new icons into tenant\n");
-                $bar->start();
-
-                $notAvailableImagesArray = [];
-                foreach ($tenantsList as $tenant) {
-
-                    // Check tenant directory is exist or not 
-                    if(Storage::disk('s3')->exists('testCommand26')){
-
-                        //check --path option directory is exist or not
-                        if(Storage::disk('s3')->exists($folderPath)){
-                            $files = Storage::disk('s3')->allFiles($folderPath);
-                            foreach($files as $key=>$file){
-
-                                // Remove default_theme path from file URL
-                                $sourcePath = str_replace(env('AWS_S3_DEFAULT_THEME_FOLDER_NAME'), '', $file);
-                                if(!Storage::disk('s3')->exists('testCommand26'.$sourcePath)){
-                                    array_push($notAvailableImagesArray, $file);
-                                    // dd($tenant['name'].$sourcePath);
-                                    // Copy and paste file into tenant's folders
-                                    Storage::disk('s3')->copy($file, 'testCommand26'.$sourcePath);
-                                    // dd('testCommand26'.$sourcePath);
-                                }
-                            }
-
-                            dd($notAvailableImagesArray);
-                        }
-                    }
-                    // $tenantName = $tenant['name'];
-                }
+                // Function for copy non existing icons 
+                $this->copyNonExistingIconPerTenant($tenants, $files, $option);
+            } else {
+                dump("Given folder path is not found");
             }
         } elseif (!empty($filePath) && empty($folderPath)) {
-            dump('file option is available');
+
+            // Check filepath is exist or not
+            if (Storage::disk('s3')->exists($filePath)) {
+                $encodedFileContent = Storage::disk('s3')->get($filePath);
+                $fileContent = json_decode($encodedFileContent);
+                $tenants = $this->tenantRepository->getAllTenants();
+                $option = 'file';
+
+                // Function for copy non existing icons
+                $this->copyNonExistingIconPerTenant($tenants, $fileContent->files, $option);
+            } else {
+                dump("Given filepath is not found.");
+            }
         } else {
             if (!empty($filePath) && !empty($folderPath)) {
-                dump('path and file both options are available');
+                dump('Only single option is acceptable at a time');
             } else {
                 dump('path or file option is missing');
             }
+        }
+    }
+
+    private function copyNonExistingIconPerTenant($tenants, $files, $option)
+    {
+        if ($tenants->count() > 0) {
+            $bar = $this->output->createProgressBar($tenants->count());
+            $tenantsList = $tenants->toArray();
+            $this->info('Total tenants : '. $tenants->count());
+            $this->info("\nIt is going to save new icons into tenant\n");
+            $bar->start();
+            $notAvailableImagesArray = [];
+            foreach ($tenantsList as $tenant) {
+
+                // Check tenant directory is exist or not
+                if (Storage::disk('s3')->exists('testCommand25')) {
+                    foreach ($files as $file) {
+                        // Remove default_theme path from file URL
+                        $sourcePath = str_replace(env('AWS_S3_DEFAULT_THEME_FOLDER_NAME'), '', $file);
+                        if (!Storage::disk('s3')->exists('testCommand25'.$sourcePath)) {
+                            array_push($notAvailableImagesArray, $file);
+
+                            // Copy and paste file into tenant's folders
+                            Storage::disk('s3')->copy($file, 'testCommand25'.$sourcePath);
+                        }
+                    }
+                }
+
+                $bar->advance();
+            }
+
+            dd("finish");
+            $bar->finish();
+            $this->info("\n \nAll non existing icons are copied from default_theme to tenant");
         }
     }
 }
