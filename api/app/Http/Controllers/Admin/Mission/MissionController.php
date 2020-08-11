951d548f5b4b3971598b904bf2c336e804eb068d
krishna <krishna.dhanani@tatvasoft.com>
2020-08-11 10:18:35 +0530

    validation changes

--- api/app/Http/Controllers/Admin/Mission/MissionController.php
+++ api/app/Http/Controllers/Admin/Mission/MissionController.php
@@ -275,6 +275,9 @@ class MissionController extends Controller
                 "volunteering_attribute.availability_id" => "sometimes|required_with:volunteering_attribute|integer|
                 exists:availability,availability_id,deleted_at,NULL",
                 "skills.*.skill_id" => "integer|exists:skill,skill_id,deleted_at,NULL",
+                "is_virtual" => "sometimes|required|in:0,1",
+				"total_seats" => "integer|min:1",
+                "availability_id" => "sometimes|required|integer|exists:availability,availability_id,deleted_at,NULL",
                 "theme_id" => "sometimes|integer|exists:mission_theme,mission_theme_id,deleted_at,NULL",
                 "application_deadline" => "date",
                 "mission_detail.*.short_description" => "max:1000",
