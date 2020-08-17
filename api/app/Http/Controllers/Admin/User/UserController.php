69c8b8d4d254b11fc72f500aa36686d1b9812252
orann <oweber@int.optimy.com>
2020-08-17 16:51:47 +0200

    [CIP-683] Corrected typo in validation

--- api/app/Http/Controllers/Admin/User/UserController.php
+++ api/app/Http/Controllers/Admin/User/UserController.php
@@ -248,7 +248,7 @@ class UserController extends Controller
             "availability_id" => "sometimes|required|integer|exists:availability,availability_id,deleted_at,NULL",
             "timezone_id" => "sometimes|required|integer|exists:timezone,timezone_id,deleted_at,NULL",
             "language_id" => "sometimes|required|int",
-            "city_id" => "|sometimes|integer|exists:city,city_id,deleted_at,NULL",
+            "city_id" => "sometimes|integer|exists:city,city_id,deleted_at,NULL",
             "country_id" => "integer|sometimes|required|exists:country,country_id,deleted_at,NULL",
             "profile_text" => "sometimes|required",
             "employee_id" => "max:16|
