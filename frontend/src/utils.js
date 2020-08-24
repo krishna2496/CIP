fa1ac438f592a9399a9dee103e6df8999fd349b4
liezl <lsantos@optimy.com>
2020-08-24 13:06:28 +0800

    [CIP-705] Simplify logic and add a fallback if site title lable key does not exist on translations array

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,39 +1,18 @@
 import store from './store';
 
 export function setSiteTitle() {
-  const languageData = JSON.parse(store.state.languageLabel);
-  if (store.state.defaultLanguage && languageData) {
-      let defaultLang = store.state.defaultLanguage.toLowerCase();
-      let siteTitle = '';
-      if (store.state.siteTitle && store.state.siteTitle.translations != "") {
-          let siteTranslationArray = store.state.siteTitle.translations;
-          let data = siteTranslationArray.filter((item) => {
-              if (item.lang == defaultLang) {
-                  return item;
-              }
-          });
-          if (data[0] && data[0].title) {
-              siteTitle = data[0].title;
-          } else {
-              let data = siteTranslationArray.filter((item) => {
-                  if (item.lang == store.state.defaultTenantLanguage.toLowerCase()) {
-                      return item;
-                  }
-              });
-
-              if (data[0] && data[0].title) {
-                  siteTitle = data[0].title;
-              } else {
-                  if (typeof(languageData.label.site_title) != "undefined") {
-                      siteTitle = languageData.label.site_title;
-                  }
-              }
-          }
-      } else {
-          if (typeof(languageData.label.site_title) != "undefined") {
-              siteTitle = languageData.label.site_title;
-          }
-      }
-      document.title = siteTitle;
+  const defaultLang = store.state.defaultLanguage.toLowerCase();
+  const translations = JSON.parse(store.state.languageLabel);
+  let siteTitle = translations.label.site_title || 'Optimy';
+  if (store.state.siteTitle
+    && store.state.siteTitle.translations
+    && store.state.siteTitle.translations.length
+  ) {
+    const siteTranslationArray = store.state.siteTitle.translations;
+    const data = siteTranslationArray.find((item) => item.lang === defaultLang);
+    if (data && data.title) {
+      siteTitle = data.title;
+    }
   }
+  document.title = siteTitle;
 }
