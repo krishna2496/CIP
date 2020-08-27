d40639e3b01aa9a221174a9d7987c4cae3048616
Mario (BU) Rey II <mreybu@optimy.com>
2020-08-28 01:31:22 +0800

    since there are no models defined that pertaining to tenants specific settings. major refactoring removing the use of DTO and repository classes, instead this is using query builder to generate SQL queries and manipulate table records directly.

--- admin-api/bin/php/oneShot/CIP-710-TenantSettingsDedupe/run.php
+++ admin-api/bin/php/oneShot/CIP-710-TenantSettingsDedupe/run.php
@@ -3,156 +3,314 @@
 require_once(__DIR__.'/../OneShot.php');
 
 
-use App\Models\Tenant;
-use App\Models\TenantHasSetting;
-use App\Models\TenantSetting;
-use App\Repositories\Tenant\TenantRepository;
-use App\Repositories\TenantHasSetting\TenantHasSettingRepository;
-use App\Repositories\TenantSetting\TenantSettingRepository;
+use Carbon\Carbon;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Collection;
 use optimy\console\OneShot;
 
 
 class TenantSettingsDedupe extends OneShot
 {
-    /**
-     * @var App\Repositories\Tenant\TenantRepository
-     */
-    private $tenantRepository;
+    const TENANT_TABLE = 'tenant';
+    const TENANT_SETTING_TABLE = 'tenant_setting';
+    const TENANT_HAS_SETTING_TABLE = 'tenant_has_setting';
+    const TENANT_ACTIVATED_SETTING_TABLE = 'tenant_activated_setting';
 
     /**
-     * @var App\Repositories\TenantSetting\TenantSettingRepository
+     * @var array
      */
-    private $tenantSettingRepository;
+    private $errorsFound = [];
 
     /**
-     * @var App\Repositories\TenantHasSetting\TenantHasSettingRepository
-     */
-    private $tenantHasSettingRepository;
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct(
-        TenantRepository $tenantRepository,
-        TenantSettingRepository $tenantSettingRepository,
-        TenantHasSetting $tenantHasSetting,
-        TenantHasSettingRepository $tenantHasSettingRepository
-    ) {
-        parent::__construct();
-        $this->tenantRepository = $tenantRepository;
-        $this->tenantSettingRepository = $tenantSettingRepository;
-        $this->tenantHasSetting = $tenantHasSetting;
-        $this->tenantHasSettingRepository = $tenantHasSettingRepository;
-    }
-
-    /**
-     * OneShot script entry point method.
+     * Execute the console command.
      *
      * @return mixed
      */
-    public function start($input = null, $output = null)
+    public function start()
     {
-        $redundantSettings = $this->getRedundantSettings($this->tenantSettingRepository->getAllSettings());
-        if (!$redundantSettings->count()) {
-            return $this->info('There are no duplicate settings to process.');
-        }
+        $this->writeLn('Removing duplicate tenant settings for all existing tenants.');
 
-        $tenants = $this->tenantRepository->getAllTenants();
+        $tenants = $this->getTenants();
         if (!$tenants->count()) {
-            return $this->info('There are no available tenants to process. ');
+            return $this->warn('There are no available tenants to process.');
         }
 
-        $this->info('Removing duplicate tenant settings for all existing tenants.');
-
-        $firstIdPerKey = [];
-        foreach ($redundantSettings as $settingKey => $settings) {
-            // get the extraneous IDs used per tenant setting key.
-            $firstIdPerKey[$settingKey] = min($settings->keys()->all());
+        $redundantMasterSettings = $this->getRedundantMasterSettings();
+        if (!$redundantMasterSettings->count()) {
+            return $this->warn('There are no duplicate settings to process.');
         }
 
-        // check if there is redundant settings for each tenant.
-        foreach ($tenants as $tenant) {
-            $this->progress();
-            try {
-                $dupeTenantSettings = $this->getRedundantSettings(
-                    $this->tenantHasSettingRepository->getSettingsList($tenant->tenant_id)
-                );
-                // iterate through the discovered redundant keys
-                foreach ($dupeTenantSettings as $settingKey => $tenantSettings) {
-                    $this->progress();
-                    $enabledSettings = $tenantSettings->count();
-                    $disableSettings = $tenantSettings->whereNotNull('deleted_at')->count();
-                    if (($enabledSettings - $disableSettings) > 0) {
-                        $initialSetting = $tenantSettings->get($firstIdPerKey[$settingKey]);
-                        if (!$initialSetting->is_active) {
-                            // enable the tenant setting if there is at least one setting that is active.
-                            $this->tenantHasSetting->enableSetting($tenant->tenant_id, $firstIdPerKey[$settingKey]);
-                        }
-                    }
-                    $excessSettings = $tenantSettings->where('tenant_setting_id', '!=', $firstIdPerKey[$settingKey]);
-                    foreach ($excessSettings as $setting) {
-                        $this->progress();
-                        // disable the remaining redundant setting.
-                        $this->tenantHasSetting->disableSetting($tenant->tenant_id, $setting->tenant_setting_id);
-                    }
-                }
-            } catch (Exception $e) {
-                $this->progress(PHP_EOL);
-                $this->warn($e->getTraceAsString());
-                throw $e;
-            }
+        $this->removeRedundantMasterSettings($tenants, $redundantMasterSettings);
+        $this->removeRedundantTenantSettings($tenants, $redundantMasterSettings);
+        $this->writeLn();
 
+        if (count($this->errorsFound)) {
+            $this->writeLn('Processing all tenants finished with errors!');
+            $this->writeLn(str_repeat('-', 40));
+            $this->warn(sprintf(implode("\n", $this->errorsFound)));
+        } else {
+            $this->writeLn('Processing all tenants finished successfully!');
         }
-
-        // Remove the redundant from the main tenant settings.
-        foreach ($redundantSettings as $settingKey => $settings) {
-            $excessSettings = $settings->where('tenant_setting_id', '!=', $firstIdPerKey[$settingKey]);
-            foreach ($excessSettings as $redundantSetting) {
-                $this->progress();
-                $redundantSetting->delete();
-            }
-        }
-
-        $this->progress(PHP_EOL);
-        $this->info('Processing all tenants finished successfully!');
     }
 
-    /**
-     * Get duplicate settings by a setting's key value
-     * @codeCoverageIgnore
-     *
-     * @return Collection
-     */
-    private function getRedundantSettings(Collection $settings): Collection
+    private function removeRedundantMasterSettings(Collection $tenants, Collection $redundantMasterSettings)
     {
-        // get all keys with duplicates.
-        $settingKeys = array_keys(array_filter(array_count_values(
-                    $settings->map(function ($setting) {
-                        return $setting->key;
-                    })->all()),
-                    function ($count) {
-                        return $count > 1;
+        foreach ($tenants as $tenant) {
+            try {
+                foreach ($redundantMasterSettings as $setting) {
+                    // extract the first ID used for tenant setting ID
+                    $tenantSettingsFirstId = min($setting->tenant_setting_ids);
+
+                    $tenantMasterSettings = $this->getTenantMasterSettings($setting->tenant_setting_ids);
+                    $tenantHasSettings = $this->getTenantHasSettings($tenant->tenant_id, $setting->tenant_setting_ids);
+
+                    // disable all redundant master tenant settings.
+                    $redundantIds = array_diff($setting->tenant_setting_ids, [$tenantSettingsFirstId]);
+                    if (count($redundantIds)) {
+                        $this->disableMasterTenantSettings($redundantIds);
                     }
-                ));
-        // create a collection hierarchy to make it easier when processing.
-        $redundantSettings = collect();
-        foreach ($settingKeys as $settingKey) {
-            $settingsPerKey = collect();
-            foreach ($settings->where('key', $settingKey) as $setting) {
-                $settingsPerKey->put($setting->tenant_setting_id, $setting);
+
+                    // disable all redundant activated tenant settings.
+                    $this->disableTenantHasSettings($tenant->tenant_id, $setting->tenant_setting_ids);
+
+                    // if at least 1 of the redundant master setting is active for a key,
+                    // then check if it has a corresponding setting activated for this tenant.
+                    $enabledSettings = $tenantMasterSettings->count();
+                    $disabledSettings = $tenantMasterSettings->whereNotNull('deleted_at')->count();
+                    if (($enabledSettings - $disabledSettings) > 0 && $tenantHasSettings->count()) {
+                        // the available mmaster setting has an activated tenant setting.
+                        // check if first ID exists, add if it is not, activate if it exists.
+                        if ($tenantHasSettings->has($tenantSettingsFirstId)) {
+                            $this->enableTenantHasSettings($tenant->tenant_id, $tenantSettingsFirstId);
+                        } else {
+                            $this->addTenantHasSettings($tenant->tenant_id, $tenantSettingsFirstId);
+                        }
+                    }
+                }
+            } catch (Exception $exception) {
+                $this->errorsFound[] = $this->formatError($exception);
             }
-            $redundantSettings->put($settingKey, $settingsPerKey);
         }
-        return $redundantSettings;
+    }
+
+    private function removeRedundantTenantSettings(Collection $tenants, Collection $redundantMasterSettings)
+    {
+        foreach ($tenants as $tenant) {
+            try {
+                $this->connectTenantDb($tenant->tenant_id);
+
+                foreach ($redundantMasterSettings as $setting) {
+                    // extract the first ID used for tenant setting ID
+                    $masterSettingsFirstId = min($setting->tenant_setting_ids);
+
+                    $tenantAvailableSettings = $this->getTenantAvailableSettings($setting->tenant_setting_ids);
+                    $tenantAvailableSettingsIds = $tenantAvailableSettings->keys()->all();
+
+                    if ($tenantAvailableSettings->count()) {
+                        // disable all available tenant settings entries.
+                        $this->disableTenantAvailableSettings($tenantAvailableSettingsIds);
+                    }
+
+                    $tenantActivatedSettings = $this->getTenantActivatedSettings($tenantAvailableSettingsIds);
+                    $tenantActivatedSettingsIds = $tenantActivatedSettings->keys()->all();
+
+                    if (!$tenantAvailableSettings->count() && !$tenantActivatedSettings->count()) {
+                        continue;
+                    }
+
+                    if ($tenantActivatedSettings->count()) {
+                        // disable all activated tenant settings entries.
+                        $this->disableTenantActivatedSettings($tenantActivatedSettingsIds);
+                    }
+
+                    // reinsert entry if tenant setting was available for tenant, and if it was activated.
+                    if ($tenantAvailableSettings->count()) {
+                        $newTenantSettingId = $this->addTenantAvailableSetting($masterSettingsFirstId);
+                        if ($tenantActivatedSettings->count()) {
+                            $newTenantActivatedSettingId = $this->addTenantActivatedSetting($newTenantSettingId);
+                        }
+                    }
+                }
+
+                $this->disconnectTenantDb();
+            } catch (Exception $exception) {
+                $this->errorsFound[] = $this->formatError($exception);
+            }
+        }
+    }
+
+    private function getDbTable(string $tableName): Builder
+    {
+        $this->progress();
+        return DB::table($tableName);
+    }
+
+    private function getTenants(): Collection
+    {
+        return $this->getDbTable(self::TENANT_TABLE)
+            ->whereNull('deleted_at')
+            ->get()
+            ->keyBy('tenant_id');
+    }
+
+    private function getRedundantMasterSettings(): Collection
+    {
+        $settings = $this->getDbTable(self::TENANT_SETTING_TABLE)
+            ->select([
+                'key',
+                DB::raw('count(`key`) group_count'),
+                DB::raw('group_concat(tenant_setting_id) tenant_setting_ids'),
+            ])
+            ->whereNull('deleted_at')
+            ->groupBy('key')
+            ->having('group_count', '>', 1)
+            ->get()
+            ->keyBy('key');
+        $settings->map(function($setting) {
+            // convert the comma separated IDs to sorted array
+            $setting->tenant_setting_ids = array_unique(explode(',', $setting->tenant_setting_ids));
+            sort($setting->tenant_setting_ids);
+        });
+        return $settings;
+    }
+
+    private function getTenantMasterSettings(array $tenantSettingIds): Collection
+    {
+        return $this->getDbTable(self::TENANT_SETTING_TABLE)
+            ->select([
+                'key',
+                'tenant_setting_id',
+                'deleted_at',
+            ])
+            ->whereIn('tenant_setting_id', $tenantSettingIds)
+            ->orderBy('tenant_setting_id')
+            ->get()
+            ->keyBy('tenant_setting_id');
+    }
+
+    private function getTenantHasSettings(int $tenantId, array $tenantSettingIds): Collection
+    {
+        return $this->getDbTable(self::TENANT_HAS_SETTING_TABLE)
+            ->select([
+                'tenant_id',
+                'tenant_setting_id',
+                'deleted_at',
+            ])
+            ->where('tenant_id', $tenantId)
+            ->whereIn('tenant_setting_id', $tenantSettingIds)
+            ->orderBy('tenant_id')
+            ->orderBy('tenant_setting_id')
+            ->get()
+            ->keyBy('tenant_setting_id');
+    }
+
+    private function disableMasterTenantSettings(array $tenantSettingIds): int
+    {
+        return $this->getDbTable(self::TENANT_SETTING_TABLE)
+            ->whereNull('deleted_at')
+            ->whereIn('tenant_setting_id', $tenantSettingIds)
+            ->update(['deleted_at' => Carbon::now()]);
+    }
+
+    private function addTenantHasSettings(int $tenantId, int $tenantSettingId)
+    {
+        $this->getDbTable(self::TENANT_HAS_SETTING_TABLE)
+            ->insert([
+                'tenant_id' => $tenantId,
+                'tenant_setting_id' => $tenantSettingId,
+            ]);
+    }
+
+    private function enableTenantHasSettings(int $tenantId, int $tenantSettingId)
+    {
+        $this->getDbTable(self::TENANT_HAS_SETTING_TABLE)
+            ->whereNotNull('deleted_at')
+            ->where('tenant_id', $tenantId)
+            ->where('tenant_setting_id', $tenantSettingId)
+            ->take(1)
+            ->update(['deleted_at' => null]);
+    }
+
+    private function disableTenantHasSettings(int $tenantId, array $tenantSettingIds)
+    {
+        return $this->getDbTable(self::TENANT_HAS_SETTING_TABLE)
+            ->whereNull('deleted_at')
+            ->where('tenant_id', $tenantId)
+            ->whereIn('tenant_setting_id', $tenantSettingIds)
+            ->update(['deleted_at' => Carbon::now()]);
+    }
+
+    private function getTenantAvailableSettings(array $settingIds): Collection
+    {
+        return $this->getDbTable(self::TENANT_SETTING_TABLE)
+            ->select([
+                'setting_id',
+                'tenant_setting_id',
+            ])
+            ->whereNull('deleted_at')
+            ->whereIn(
+                'setting_id',
+                $settingIds
+            )
+            ->orderBy('setting_id')
+            ->orderBy('tenant_setting_id')
+            ->get()
+            ->keyBy('tenant_setting_id');
+    }
+
+    private function addTenantAvailableSetting(int $settingId): int
+    {
+        return $this->getDbTable(self::TENANT_SETTING_TABLE)
+            ->insertGetId(['setting_id' => $settingId]);
+    }
+
+    private function disableTenantAvailableSettings(array $tenantSettingIds)
+    {
+        return $this->getDbTable(self::TENANT_SETTING_TABLE)
+            ->whereNull('deleted_at')
+            ->whereIn(
+                'tenant_setting_id',
+                $tenantSettingIds
+            )
+            ->update(['deleted_at' => Carbon::now()]);
+    }
+
+    private function getTenantActivatedSettings(array $tenantSettingIds): Collection
+    {
+        return $this->getDbTable(self::TENANT_ACTIVATED_SETTING_TABLE)
+            ->select([
+                'tenant_activated_setting_id',
+                'tenant_setting_id',
+            ])
+            ->whereNull('deleted_at')
+            ->whereIn(
+                'tenant_setting_id',
+                $tenantSettingIds
+            )
+            ->get()
+            ->keyBy('tenant_activated_setting_id');
+    }
+
+    private function addTenantActivatedSetting(int $tenantSettingId): int
+    {
+        return $this->getDbTable(self::TENANT_ACTIVATED_SETTING_TABLE)
+            ->insertGetId(['tenant_setting_id' => $tenantSettingId]);
+    }
+
+    private function disableTenantActivatedSettings(array $tenantActivatedSettingIds)
+    {
+        return $this->getDbTable(self::TENANT_ACTIVATED_SETTING_TABLE)
+            ->whereNull('deleted_at')
+            ->whereIn(
+                'tenant_activated_setting_id',
+                $tenantActivatedSettingIds
+            )
+            ->update(['deleted_at' => Carbon::now()]);
     }
 }
 
+
 // instantiate TenantSettingsDedupe class with the application
-// container which should automagically inject all the required
-// dependency to its constructor and finally make a call to our
-// starting entry point method.
+// container and then make a call to the entry point method.
 $cli = $app->make(TenantSettingsDedupe::class);
 $cli->start();
