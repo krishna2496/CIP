0c467e2ebd472c23addeee9043231fa6893c370f
Dhaval Prajapati <dhaval.prajapati@tatvasoft.com>
2020-09-02 10:32:14 +0530

    remove unnecessary changes

--- admin-api/database/seeds/VolunteeringTimeAndGoalMissionTenantSettingSeeder.php
+++ admin-api/database/seeds/VolunteeringTimeAndGoalMissionTenantSettingSeeder.php
@@ -17,15 +17,13 @@ class VolunteeringTimeAndGoalMissionTenantSettingSeeder extends Seeder
                 'title' => 'Volunteering time mission',
                 'description' => 'Enabled/disabled Volunteering time mission',
                 'key' => 'volunteering_time_mission',
-                "created_at" => Carbon::now(),
-                "updated_at" => Carbon::now()
+                "created_at" => Carbon::now()
             ],
             [
                 'title' => 'Volunteering goal mission',
                 'description' => 'Enabled/disabled Volunteering goal mission',
                 'key' => 'volunteering_goal_mission',
-                "created_at" => Carbon::now(),
-                "updated_at" => Carbon::now()
+                "created_at" => Carbon::now()
             ]
         ];
 
