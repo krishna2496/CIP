67b52a1842dc5ecd46f27d0df58e90bdec00e564
Bastien JEAN <bjean@int.optimy.com>
2020-09-14 17:02:30 +0200

    CIP-674 removed useless checks in oneShot scripts

--- admin-api/bin/php/oneShot/CIP-674-ConvertSkillTranslationsToJson/rollback.php
+++ admin-api/bin/php/oneShot/CIP-674-ConvertSkillTranslationsToJson/rollback.php
@@ -8,51 +8,47 @@ $pdo = $db->connection('mysql')->getPdo();
 \Illuminate\Support\Facades\Config::set('database.default', 'mysql');
 $tenants = $pdo->query('select * from tenant where status = 1 and deleted_at IS NULL')->fetchAll();
 
-if (count($tenants) > 0) {
-    foreach ($tenants as $tenant) {
-        $tenantId = $tenant['tenant_id'];
-        $db->purge('tenant');
-        // Create connection to tenant
-        \Illuminate\Support\Facades\Config::set('database.connections.tenant', array(
-            'driver' => 'mysql',
-            'host' => env('DB_HOST'),
-            'database' => 'ci_tenant_'.$tenantId,
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-        ));
-        // Create connection for the tenant database
-        $pdo = $db->connection('tenant')->getPdo();
-        $pdo->exec('SET NAMES utf8mb4');
-        $pdo->exec('SET CHARACTER SET utf8mb4');
+foreach ($tenants as $tenant) {
+    $tenantId = $tenant['tenant_id'];
+    $db->purge('tenant');
+    // Create connection to tenant
+    \Illuminate\Support\Facades\Config::set('database.connections.tenant', array(
+        'driver' => 'mysql',
+        'host' => env('DB_HOST'),
+        'database' => 'ci_tenant_'.$tenantId,
+        'username' => env('DB_USERNAME'),
+        'password' => env('DB_PASSWORD'),
+    ));
+    // Create connection for the tenant database
+    $pdo = $db->connection('tenant')->getPdo();
+    $pdo->exec('SET NAMES utf8mb4');
+    $pdo->exec('SET CHARACTER SET utf8mb4');
 
-        // Set default database
-        \Illuminate\Support\Facades\Config::set('database.default', 'tenant');
-        $skills = $pdo->query('select skill_id, translations from skill')->fetchAll();
+    // Set default database
+    \Illuminate\Support\Facades\Config::set('database.default', 'tenant');
+    $skills = $pdo->query('select skill_id, translations from skill')->fetchAll();
 
-        if (!empty($skills)) {
-            foreach ($skills as $skill) {
+    foreach ($skills as $skill) {
 
-                $skillTranslations = json_decode($skill['translations'], true);
+        $skillTranslations = json_decode($skill['translations'], true);
 
-                $trans = [];
-                foreach ($skillTranslations as $key => $translation) {
-                    $trans[] = [
-                        'lang' => $key,
-                        'title' => $translation
-                    ];
-                }
-
-                $translations = json_encode($trans, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-                $pdo->prepare('
-                        UPDATE skill
-                        SET translations = :translations
-                        WHERE skill_id = :id
-                    ')
-                    ->execute([
-                        'translations' => $translations,
-                        'id' => $skill['skill_id'],
-                    ]);
-            }
+        $trans = [];
+        foreach ($skillTranslations as $key => $translation) {
+            $trans[] = [
+                'lang' => $key,
+                'title' => $translation
+            ];
         }
+
+        $translations = json_encode($trans, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $pdo->prepare('
+                UPDATE skill
+                SET translations = :translations
+                WHERE skill_id = :id
+            ')
+            ->execute([
+                'translations' => $translations,
+                'id' => $skill['skill_id'],
+            ]);
     }
 }
