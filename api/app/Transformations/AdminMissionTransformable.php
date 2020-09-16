15bf295084cf2959dec320d3edfc4c75669e45a9
Ravi chauahan <ravi.chauhan@tatvasoft.com>
2020-09-16 17:00:25 +0530

    client comment updated

--- api/app/Transformations/AdminMissionTransformable.php
+++ api/app/Transformations/AdminMissionTransformable.php
@@ -4,6 +4,8 @@ namespace App\Transformations;
 use App\Models\Mission;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
+use App\Repositories\TenantActivatedSetting\TenantActivatedSettingRepository;
+use Illuminate\Http\Request;
 
 trait AdminMissionTransformable
 {
@@ -16,32 +18,42 @@ trait AdminMissionTransformable
      */
     protected function adminTransformMission(
         $mission,
-        Collection $languages
+        Collection $languages,
+        TenantActivatedSettingRepository $tenantActivatedSettingRepository
     ) {
         // Transform impact mission attribute
+        $request = new Request();
+        $missionImpactSettingActivated = $tenantActivatedSettingRepository->checkTenantSettingStatus(
+            config('constants.tenant_settings.MISSION_IMPACT'),
+            $request
+        );
 
-        if (isset($mission['impact'])) {
-            $impactMission =  $mission['impact']->toArray();
-            if ($impactMission != null) {
-                $impactMissionDetails = [];
-                foreach ($impactMission as $impactMissionKey => $impactMissionValue) {
-                    $impactMissionDetails['mission_impact_id'] = $impactMissionValue['mission_impact_id'];
-                    $impactMissionDetails['sort_key'] = $impactMissionValue['sort_key'];
-                    $impactMissionDetails['icon_path'] = $impactMissionValue['icon_path'];
-                    $impactMissionDetails['translations'] = [];
-                    foreach ($impactMissionValue['mission_impact_language_details'] as $impactMissionLanguageValue) {
-                        $languageCode = $languages
-                            ->where('language_id', $impactMissionLanguageValue['language_id'])
-                            ->first()
-                            ->code;
-                        $impactMissionLanguage['language_id'] = $impactMissionLanguageValue['language_id'];
-                        $impactMissionLanguage['language_code'] = $languageCode;
-                        $impactMissionLanguage['content'] = json_decode($impactMissionLanguageValue['content']);
-                        array_push($impactMissionDetails['translations'], $impactMissionLanguage);
+        if($missionImpactSettingActivated){
+            if (isset($mission['impact'])) {
+                $impactMission =  $mission['impact']->toArray();
+                if ($impactMission != null) {
+                    $impactMissionDetails = [];
+                    foreach ($impactMission as $impactMissionKey => $impactMissionValue) {
+                        $impactMissionDetails['mission_impact_id'] = $impactMissionValue['mission_impact_id'];
+                        $impactMissionDetails['sort_key'] = $impactMissionValue['sort_key'];
+                        $impactMissionDetails['icon_path'] = $impactMissionValue['icon_path'];
+                        $impactMissionDetails['translations'] = [];
+                        foreach ($impactMissionValue['mission_impact_language_details'] as $impactMissionLanguageValue) {
+                            $languageCode = $languages
+                                ->where('language_id', $impactMissionLanguageValue['language_id'])
+                                ->first()
+                                ->code;
+                            $impactMissionLanguage['language_id'] = $impactMissionLanguageValue['language_id'];
+                            $impactMissionLanguage['language_code'] = $languageCode;
+                            $impactMissionLanguage['content'] = json_decode($impactMissionLanguageValue['content']);
+                            array_push($impactMissionDetails['translations'], $impactMissionLanguage);
+                        }
+                        $mission['impact'][$impactMissionKey] = $impactMissionDetails;
                     }
-                    $mission['impact'][$impactMissionKey] = $impactMissionDetails;
                 }
             }
+        } else {
+            unset($mission['impact']);
         }
 
         return $mission;
