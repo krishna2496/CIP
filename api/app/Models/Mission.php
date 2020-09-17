1177773bf56c068b89feb4f18a51246676657449
liezl <lsantos@optimy.com>
2020-09-17 12:42:07 +0800

    [CIP-653] Put back total_seats for mission app GET endpoint

--- api/app/Models/Mission.php
+++ api/app/Models/Mission.php
@@ -88,7 +88,7 @@ class Mission extends Model
     'availability_id', 'availability_type', 'average_rating', 'timesheet', 'total_hours', 'time',
     'hours', 'action', 'ISO', 'total_minutes', 'custom_information', 'total_timesheet_time', 'total_timesheet_action', 'total_timesheet',
     'mission_title', 'mission_objective', 'label_goal_achieved', 'label_goal_objective', 'state', 'state_name', 'organization', 'organization_name', 'missionTab', 'volunteeringAttribute',
-    'unSdg', 'is_virtual'];
+    'unSdg', 'is_virtual', 'total_seats'];
 
     /*
      * Iatstuti\Database\Support\CascadeSoftDeletes;
