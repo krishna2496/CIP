ee92d2f91c16668e6d6f37587fa99de586e6e7c1
krishna <krishna.dhanani@tatvasoft.com>
2020-09-18 13:09:31 +0530

    is active changes

--- api/app/Helpers/Helpers.php
+++ api/app/Helpers/Helpers.php
@@ -557,7 +557,7 @@ class Helpers
         $tenantLanguage = $this->db->table('tenant_currency')
         ->where('tenant_id', $tenant->tenant_id)
         ->where('code', $currencyCode)
-        ->where('is_active', 1);
+        ->where('is_active', '1');
 
         // Connect tenant database
         $this->switchDatabaseConnection('tenant');
