82a9933bd350cc1ba929b2623866c37c92f45d1c
Quentin Delcourt <qdelcourt@optimy.com>
2020-09-18 13:55:15 +0200

    [CIP-769] JWT should now be stored in a cookie, not in a general header

--- api/app/Http/Middleware/JwtMiddleware.php
+++ api/app/Http/Middleware/JwtMiddleware.php
@@ -7,6 +7,7 @@ use Firebase\JWT\JWT;
 use App\User;
 use App\Helpers\ResponseHelper;
 use Firebase\JWT\ExpiredException;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Helpers\Helpers;
 use App\Repositories\Timezone\TimezoneRepository;
@@ -49,13 +50,13 @@ class JwtMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  object  $request
+     * @param  Request  $request
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle(Request $request, Closure $next, $guard = null)
     {
-        $token = ($request->hasHeader('token')) ? $request->header('token') : '';
+        $token = ($request->hasCookie('token')) ? $request->cookie('token') : '';
 
         if (!$token) {
             // Unauthorized response if token not there
