2b13324967c36fbb47b5bba1638dd5ab31def6a8
Ravi chauahan <ravi.chauhan@tatvasoft.com>
2020-09-21 17:06:29 +0530

    remove mission_tab data from explore mission

--- api/app/Repositories/Mission/MissionRepository.php
+++ api/app/Repositories/Mission/MissionRepository.php
@@ -689,10 +689,6 @@ class MissionRepository implements MissionInterface
                 config('constants.timesheet_status.AUTOMATICALLY_APPROVED'), ));
             }, ]);
         $missionQuery->with(['missionRating']);
-        $missionQuery->with(['missionTabs' => function ($query) {
-            $query->orderBy('sort_key');
-        }, 'missionTabs.getMissionTabDetail' => function ($query) {
-        }]);
 
         //Explore mission recommended to user
         if ($request->has('explore_mission_type') &&
