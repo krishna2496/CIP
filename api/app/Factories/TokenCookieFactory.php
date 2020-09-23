4a6d5963cae0e5e6a726f4a3d3af94d7ed503ab2
Quentin Delcourt <qdelcourt@optimy.com>
2020-09-23 14:56:46 +0200

    [CIP-769] Implement cookie token creation method

--- api/app/Factories/TokenCookieFactory.php
+++ api/app/Factories/TokenCookieFactory.php
@@ -11,8 +11,23 @@ class TokenCookieFactory
 {
     public const TOKEN_COOKIE_NAME = 'token';
 
-    public static function make() : Cookie
+    /**
+     * @param string $token The JWT token that will be stored in the cookie
+     * @param string $referer Referer extracted from the request, from which we'll retrieve the domain
+     * @param bool $isSecured Whether the cookie should be secured or not (not enabled by default to support local env)
+     * @return Cookie
+     */
+    public static function make(string $token, string $referer, bool $isSecured) : Cookie
     {
+        return new Cookie(
+            TokenCookieFactory::TOKEN_COOKIE_NAME,
+            $token,
+            strtotime('+4hours'),
+            '/',
+            parse_url($referer, PHP_URL_HOST),
+            $isSecured,
+            true
+        );
     }
 
     public static function makeExpired() : Cookie
--- api/tests/Unit/Factories/TokenCookieFactoryTest.php
+++ api/tests/Unit/Factories/TokenCookieFactoryTest.php
@@ -8,16 +8,38 @@ use Symfony\Component\HttpFoundation\Cookie;
 
 class TokenCookieFactoryTest extends TestCase
 {
-    public function testMakeExpired()
+    public function testMake()
     {
-        $expected = new Cookie(
-            'cookiename',
-            0
-        );
+        $token = '1234567890';
+        $referer = 'http://somedomain.com:1234/foo';
+        $expectedDomain = 'somedomain.com';
+        $expectedPath = '/';
+        $isSecured = true;
 
-        $actual = TokenCookieFactory::makeExpired();
-        $this->assertEquals($expected->getExpiresTime(), $actual->getExpiresTime());
+        $actual = TokenCookieFactory::make($token, $referer, $isSecured);
+        $this->assertInstanceOf(Cookie::class, $actual);
+        $this->assertEquals($expectedDomain, $actual->getDomain());
+        $this->assertEquals($expectedPath, $actual->getPath());
+        $this->assertEquals($token, $actual->getValue());
+        $this->assertTrue($actual->isSecure());
+        $this->assertTrue($actual->isHttpOnly());
     }
 
+    public function testMakeWithNonSecureOption()
+    {
+        $token = '1234567890';
+        $referer = 'http://somedomain.com:1234/foo';
+        $isSecured = false;
 
+        $actual = TokenCookieFactory::make($token, $referer, $isSecured);
+        $this->assertFalse($actual->isSecure());
+    }
+
+    public function testMakeExpired()
+    {
+        $expectedExpirationTime = 0;
+        $actual = TokenCookieFactory::makeExpired();
+        $this->assertInstanceOf(Cookie::class, $actual);
+        $this->assertEquals($expectedExpirationTime, $actual->getExpiresTime());
+    }
 }
